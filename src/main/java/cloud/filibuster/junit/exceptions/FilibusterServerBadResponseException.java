0d0d8dfb74701f59666900bd98f21a38ae055351
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2022-10-17 15:28:37 -0400

    Add new exception type.

---
+++ src/main/java/cloud/filibuster/junit/exceptions/FilibusterServerBadResponseException.java
@@ -0,0 +1,19 @@
+package cloud.filibuster.junit.exceptions;
+
+import cloud.filibuster.exceptions.FilibusterRuntimeException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FilibusterServerBadResponseException extends FilibusterRuntimeException {
+    private static final Logger logger = Logger.getLogger(FilibusterServerBadResponseException.class.getName());
+
+    public static void logAndThrow(String message) {
+        logger.log(Level.SEVERE, message);
+        throw new FilibusterServerBadResponseException(message);
+    }
+
+    public FilibusterServerBadResponseException(String message) {
+        super(message);
+    }
+}

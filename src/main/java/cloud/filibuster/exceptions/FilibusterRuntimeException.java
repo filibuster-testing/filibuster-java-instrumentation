e6aad61becae809bca4e1d06c3c9ac9e72676332
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2022-10-17 15:28:16 -0400

    Implement superclass interface for custom runtime exception.

--- src/main/java/cloud/filibuster/exceptions/FilibusterRuntimeException.java
+++ src/main/java/cloud/filibuster/exceptions/FilibusterRuntimeException.java
@@ -1,4 +1,21 @@
 package cloud.filibuster.exceptions;
 
 public class FilibusterRuntimeException extends RuntimeException {
+    public FilibusterRuntimeException() {
+        super();
+    }
+    public FilibusterRuntimeException(String message) {
+        super(message);
+    }
+
+    public FilibusterRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    protected FilibusterRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    public FilibusterRuntimeException(Throwable cause) {
+        super(cause);
+    }
 }

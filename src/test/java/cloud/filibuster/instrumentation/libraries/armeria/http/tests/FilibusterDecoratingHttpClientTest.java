64bbcf32a9720c4b52029fdd0d489e5b04d8709a
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2022-10-25 15:14:01 -0400

    Reduce sleep to confirm.

--- src/test/java/cloud/filibuster/instrumentation/libraries/armeria/http/tests/FilibusterDecoratingHttpClientTest.java
+++ src/test/java/cloud/filibuster/instrumentation/libraries/armeria/http/tests/FilibusterDecoratingHttpClientTest.java
@@ -97,7 +97,7 @@ public class FilibusterDecoratingHttpClientTest extends FilibusterDecoratingHttp
         // race condition: join() completes at the same point that response.whenComplete() finishes
         // this is where we pop the EI, this could be a potential problem, probably need to enforce a mutex.
         // for now, hack it and sleep.
-        Thread.sleep(20);
+        Thread.sleep(15);
     }
 
     public void startExternalServer() throws InterruptedException, IOException {

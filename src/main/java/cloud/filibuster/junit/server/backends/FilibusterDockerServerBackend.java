5b2eed8d4700e1143a1c2ab848240f662ea6fc6e
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2022-11-03 23:53:27 -0400

    Set server properties.

--- src/main/java/cloud/filibuster/junit/server/backends/FilibusterDockerServerBackend.java
+++ src/main/java/cloud/filibuster/junit/server/backends/FilibusterDockerServerBackend.java
@@ -1,17 +1,28 @@
 package cloud.filibuster.junit.server.backends;
 
+import cloud.filibuster.instrumentation.helpers.Property;
 import cloud.filibuster.junit.configuration.FilibusterConfiguration;
 import cloud.filibuster.junit.server.FilibusterServerBackend;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+
+import static cloud.filibuster.instrumentation.helpers.Property.SERVER_HOST_DEFAULT;
+import static cloud.filibuster.instrumentation.helpers.Property.SERVER_PORT_DEFAULT;
 
 public class FilibusterDockerServerBackend implements FilibusterServerBackend {
     private GenericContainer<?> container;
 
+    private static final int EXPOSED_PORT = 5005;
+
     @Override
     public synchronized boolean start(FilibusterConfiguration filibusterConfiguration) throws Throwable {
-        this.container = new GenericContainer<>(DockerImageName.parse("filibuster:0.33"))
-                .withExposedPorts(5005);
+        this.container = new GenericContainer<>(DockerImageName.parse("cmeiklejohn/filibuster:0.33"))
+                .withExposedPorts(EXPOSED_PORT)
+                .waitingFor(new HttpWaitStrategy().forPort(EXPOSED_PORT))
+                .withStartupTimeout(Duration.ofMinutes(5));
 
         if (filibusterConfiguration.getSuppressCombinations()) {
             this.container = this.container.withEnv("SHOULD_SUPPRESS_COMBINATIONS", "True");
@@ -23,12 +34,19 @@ public class FilibusterDockerServerBackend implements FilibusterServerBackend {
 
         this.container.start();
 
+        Property.setServerHostProperty(container.getHost());
+        Property.setServerPortProperty(container.getMappedPort(EXPOSED_PORT));
+
         return true;
     }
 
     @Override
     public synchronized boolean stop(FilibusterConfiguration filibusterConfiguration) {
         container.stop();
+
+        Property.setServerHostProperty(SERVER_HOST_DEFAULT);
+        Property.setServerPortProperty(SERVER_PORT_DEFAULT);
+
         return true;
     }
 }

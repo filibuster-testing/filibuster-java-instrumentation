ee251b25d76b6b5dd806738a64955a694ca8b404
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-18 17:34:04 -0500

    Call super constructors for custom Runtime Exception.

--- src/main/java/cloud/filibuster/exceptions/CircuitBreakerException.java
+++ src/main/java/cloud/filibuster/exceptions/CircuitBreakerException.java
@@ -2,14 +2,14 @@ package cloud.filibuster.exceptions;
 
 public class CircuitBreakerException extends RuntimeException {
     public CircuitBreakerException(String message) {
-
+        super(message);
     }
 
     public CircuitBreakerException(String message, Throwable cause) {
-
+        super(message, cause);
     }
 
     public CircuitBreakerException(Throwable cause) {
-
+        super(cause);
     }
 }

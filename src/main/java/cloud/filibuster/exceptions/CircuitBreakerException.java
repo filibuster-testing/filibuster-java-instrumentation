a1c1669e790a3eda0a76cbad6660965426a1483a
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-18 16:49:03 -0500

    Add new exception.

---
+++ src/main/java/cloud/filibuster/exceptions/CircuitBreakerException.java
@@ -0,0 +1,15 @@
+package cloud.filibuster.exceptions;
+
+public class CircuitBreakerException extends RuntimeException {
+    public CircuitBreakerException(String message) {
+
+    }
+
+    public CircuitBreakerException(String message, Throwable cause) {
+
+    }
+
+    public CircuitBreakerException(Throwable cause) {
+
+    }
+}

88b1d5f88e817e2bd4292b8f2b4a56ce56d03803
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-23 16:43:50 -0500

    Track the analysis configurations.

--- src/main/java/cloud/filibuster/junit/configuration/FilibusterCustomAnalysisConfigurationFile.java
+++ src/main/java/cloud/filibuster/junit/configuration/FilibusterCustomAnalysisConfigurationFile.java
@@ -18,10 +18,17 @@ public class FilibusterCustomAnalysisConfigurationFile {
 
     private final JSONObject analysisConfigurationFile = new JSONObject();
 
+    private final List<FilibusterAnalysisConfiguration> filibusterAnalysisConfigurations = new ArrayList<>();
+
+    public List<FilibusterAnalysisConfiguration> getFilibusterAnalysisConfigurations() {
+        return filibusterAnalysisConfigurations;
+    }
+
     public FilibusterCustomAnalysisConfigurationFile(Builder builder) {
         for (FilibusterAnalysisConfiguration analysisConfiguration : builder.analysisConfigurations) {
             Map.Entry<String, JSONObject> entry = analysisConfiguration.toJSONPair();
             analysisConfigurationFile.put(entry.getKey(), entry.getValue());
+            filibusterAnalysisConfigurations.add(analysisConfiguration);
         }
     }
 

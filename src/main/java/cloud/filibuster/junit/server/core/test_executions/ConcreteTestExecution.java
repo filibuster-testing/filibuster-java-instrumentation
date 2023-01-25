2a6a44a9b0d5b7ce6236f5598725dac540857e70
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-25 04:05:59 -0500

    Remove unused constructor.

--- src/main/java/cloud/filibuster/junit/server/core/test_executions/ConcreteTestExecution.java
+++ src/main/java/cloud/filibuster/junit/server/core/test_executions/ConcreteTestExecution.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 // A valid test execution derived from a partial test execution once executed.
 public class ConcreteTestExecution extends TestExecution {
-
     public ConcreteTestExecution() {
 
     }
@@ -17,17 +16,4 @@ public class ConcreteTestExecution extends TestExecution {
             faultsToInject.put(faultToInject.getKey(), faultToInject.getValue());
         }
     }
-
-    public ConcreteTestExecution(PartialTestExecution partialTestExecution, boolean shouldCopyRPCs) {
-        for (Map.Entry<DistributedExecutionIndex, JSONObject> faultToInject : partialTestExecution.faultsToInject.entrySet()) {
-            faultsToInject.put(faultToInject.getKey(), faultToInject.getValue());
-        }
-
-        if (shouldCopyRPCs) {
-            for (Map.Entry<DistributedExecutionIndex, JSONObject> executedRPC : partialTestExecution.executedRPCs.entrySet()) {
-                executedRPCs.put(executedRPC.getKey(), executedRPC.getValue());
-            }
-        }
-    }
-
 }

a202753c1f9dfb924362c180993c197a66221c6b
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-25 04:08:50 -0500

    Add option for detection of divergence.

--- src/main/java/cloud/filibuster/junit/configuration/FilibusterConfiguration.java
+++ src/main/java/cloud/filibuster/junit/configuration/FilibusterConfiguration.java
@@ -34,6 +34,8 @@ public class FilibusterConfiguration {
 
     private final boolean degradeWhenServerInitializationFails;
 
+    private final boolean detectDivergence;
+
     private final Class<? extends RuntimeException> expected;
 
     private FilibusterConfiguration(Builder builder) {
@@ -45,6 +47,7 @@ public class FilibusterConfiguration {
         this.dockerImageName = builder.dockerImageName;
         this.degradeWhenServerInitializationFails = builder.degradeWhenServerInitializationFails;
         this.expected = builder.expected;
+        this.detectDivergence = builder.detectDivergence;
     }
 
     /**
@@ -63,6 +66,15 @@ public class FilibusterConfiguration {
      */
     public Class<? extends RuntimeException> getExpected() {
         return this.expected;
+    }
+
+    /**
+     * Should we detect and report divergence?
+     *
+     * @return boolean
+     */
+    public boolean getDetectDivergence() {
+        return this.detectDivergence;
     }
 
     /**
@@ -167,6 +179,8 @@ public class FilibusterConfiguration {
 
         private boolean degradeWhenServerInitializationFails = false;
 
+        private boolean detectDivergence = false;
+
         private Class<? extends RuntimeException> expected;
 
         /**
@@ -270,6 +284,18 @@ public class FilibusterConfiguration {
         @CanIgnoreReturnValue
         public Builder degradeWhenServerInitializationFails(boolean degradeWhenServerInitializationFails) {
             this.degradeWhenServerInitializationFails = degradeWhenServerInitializationFails;
+            return this;
+        }
+
+        /**
+         * Should the test detect and report divergence?
+         *
+         * @param detectDivergence boolean
+         * @return builder
+         */
+        @CanIgnoreReturnValue
+        public Builder detectDivergence(boolean detectDivergence) {
+            this.detectDivergence = detectDivergence;
             return this;
         }
 

640fc730a00f60d7486b3c2154b2d554cf705588
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-26 16:54:36 -0500

    Add new nondeterministic contains option.

--- src/main/java/cloud/filibuster/junit/server/core/TestExecutionQueue.java
+++ src/main/java/cloud/filibuster/junit/server/core/TestExecutionQueue.java
@@ -1,7 +1,18 @@
 package cloud.filibuster.junit.server.core;
 
+import cloud.filibuster.junit.server.core.test_executions.TestExecution;
+
 import java.util.concurrent.LinkedBlockingDeque;
 
-public class TestExecutionQueue<T> extends LinkedBlockingDeque<T> {
+public class TestExecutionQueue<T extends TestExecution> extends LinkedBlockingDeque<T> {
+    public boolean nondeterministicContains(TestExecution te) {
 
+        for (T t : this) {
+            if (t.nondeterministicEquals(te)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }

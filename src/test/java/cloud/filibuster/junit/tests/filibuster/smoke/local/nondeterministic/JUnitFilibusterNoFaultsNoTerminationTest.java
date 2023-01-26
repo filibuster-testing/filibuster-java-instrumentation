6ec255ed8a4b6df251403b363b1daa80ca0ac094
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-26 14:59:31 -0500

    Add new test for negative behavior of nondeterminism.

---
+++ src/test/java/cloud/filibuster/junit/tests/filibuster/smoke/local/nondeterministic/JUnitFilibusterNoFaultsNoTerminationTest.java
@@ -0,0 +1,70 @@
+package cloud.filibuster.junit.tests.filibuster.smoke.local.nondeterministic;
+
+import cloud.filibuster.examples.Hello;
+import cloud.filibuster.examples.HelloServiceGrpc;
+import cloud.filibuster.instrumentation.helpers.Networking;
+import cloud.filibuster.junit.FilibusterTest;
+import cloud.filibuster.junit.server.backends.FilibusterLocalServerBackend;
+import cloud.filibuster.junit.tests.filibuster.JUnitBaseTest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnMethod;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Test simple annotation usage.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class JUnitFilibusterNoFaultsNoTerminationTest extends JUnitBaseTest {
+    private final static Set<String> testExceptionsThrown = new HashSet<>();
+
+    private static int numberOfTestsExecuted = 0;
+
+    @DisplayName("Test partial hello server grpc route with Filibuster. (MyHelloService, MyWorldService)")
+    @FilibusterTest(serverBackend=FilibusterLocalServerBackend.class, maxIterations=10)
+    @Order(1)
+    public void testMyHelloAndMyWorldServiceWithFilibuster() throws InterruptedException {
+        ManagedChannel helloChannel = ManagedChannelBuilder
+                .forAddress(Networking.getHost("hello"), Networking.getPort("hello"))
+                .usePlaintext()
+                .build();
+
+        numberOfTestsExecuted++;
+
+        HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(helloChannel);
+        Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName("Armerian " + Math.random()).build();
+        Hello.HelloReply reply = blockingStub.partialHello(request);
+        assertTrue(reply.getMessage().contains("Hello, Armerian"));
+
+        helloChannel.shutdownNow();
+        helloChannel.awaitTermination(1000, TimeUnit.SECONDS);
+    }
+
+    @DisplayName("Verify correct number of generated Filibuster tests.")
+    @Test
+    @Order(2)
+    public void testNumAssertions() {
+        // No faults were injected.
+        assertEquals(0, testExceptionsThrown.size());
+    }
+
+    @DisplayName("Verify correct number of executed tests.")
+    @Test
+    @Order(3)
+    public void testNumberOfTestsExecuted() {
+        // maxIterations hit because of no termination.
+        assertEquals(10, numberOfTestsExecuted);
+    }
+}

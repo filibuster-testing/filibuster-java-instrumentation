f6b904925f93c00c5b01454e783ae61da7200531
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-29 16:09:27 -0500

    Add new abstract helpers and constructors.

--- src/main/java/cloud/filibuster/junit/server/core/test_executions/AbstractTestExecution.java
+++ src/main/java/cloud/filibuster/junit/server/core/test_executions/AbstractTestExecution.java
@@ -3,7 +3,27 @@ package cloud.filibuster.junit.server.core.test_executions;
 import cloud.filibuster.dei.DistributedExecutionIndex;
 import org.json.JSONObject;
 
+import javax.annotation.Nullable;
+
 public class AbstractTestExecution extends TestExecution {
+    @Nullable
+    private ConcreteTestExecution sourceConcreteTestExecution;
+
+    public AbstractTestExecution() {
+    }
+
+    public AbstractTestExecution(ConcreteTestExecution concreteTestExecution) {
+        this.sourceConcreteTestExecution = concreteTestExecution;
+    }
+
+    public boolean sawInConcreteTestExecution(DistributedExecutionIndex distributedExecutionIndex) {
+        if (sourceConcreteTestExecution != null) {
+            return sourceConcreteTestExecution.executedRPCs.containsKey(distributedExecutionIndex);
+        }
+
+        return false;
+    }
+
     public void addFaultToInject(DistributedExecutionIndex distributedExecutionIndex, JSONObject faultObject) {
         faultsToInject.put(distributedExecutionIndex, faultObject);
     }

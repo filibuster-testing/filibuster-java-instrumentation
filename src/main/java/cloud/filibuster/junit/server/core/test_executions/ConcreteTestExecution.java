df088b0dd9d07b40790a96d3fd12cbd8c26c7194
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-29 16:04:13 -0500

    Add new concrete -> abstract conversion function.

--- src/main/java/cloud/filibuster/junit/server/core/test_executions/ConcreteTestExecution.java
+++ src/main/java/cloud/filibuster/junit/server/core/test_executions/ConcreteTestExecution.java
@@ -15,4 +15,23 @@ public class ConcreteTestExecution extends TestExecution {
             faultsToInject.put(faultToInject.getKey(), faultToInject.getValue());
         }
     }
+
+    @SuppressWarnings("Varifier")
+    public AbstractTestExecution toAbstractTestExecution() {
+        AbstractTestExecution abstractTestExecution = new AbstractTestExecution(this);
+
+        for (Map.Entry<DistributedExecutionIndex, JSONObject> mapEntry : executedRPCs.entrySet()) {
+            abstractTestExecution.executedRPCs.put(mapEntry.getKey(), mapEntry.getValue());
+        }
+
+        for (Map.Entry<DistributedExecutionIndex, JSONObject> mapEntry : nondeterministicExecutedRPCs.entrySet()) {
+            abstractTestExecution.nondeterministicExecutedRPCs.put(mapEntry.getKey(), mapEntry.getValue());
+        }
+
+        for (Map.Entry<DistributedExecutionIndex, JSONObject> mapEntry : faultsToInject.entrySet()) {
+            abstractTestExecution.faultsToInject.put(mapEntry.getKey(), mapEntry.getValue());
+        }
+
+        return abstractTestExecution;
+    }
 }

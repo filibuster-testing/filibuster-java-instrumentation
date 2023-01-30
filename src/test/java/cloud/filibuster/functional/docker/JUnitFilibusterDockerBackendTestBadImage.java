3635283a4f564d0922387a1cfa0a0861e4aae819
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-01-30 16:48:48 -0500

    Add server backend.

--- src/test/java/cloud/filibuster/functional/docker/JUnitFilibusterDockerBackendTestBadImage.java
+++ src/test/java/cloud/filibuster/functional/docker/JUnitFilibusterDockerBackendTestBadImage.java
@@ -6,6 +6,7 @@ import cloud.filibuster.instrumentation.helpers.Networking;
 import cloud.filibuster.junit.FilibusterTest;
 import cloud.filibuster.junit.interceptors.GitHubActionsSkipInvocationInterceptor;
 import cloud.filibuster.functional.JUnitBaseTest;
+import cloud.filibuster.junit.server.backends.FilibusterDockerServerBackend;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.DisplayName;
@@ -41,7 +42,7 @@ public class JUnitFilibusterDockerBackendTestBadImage extends JUnitBaseTest {
      * @throws InterruptedException if teardown of gRPC channel fails.
      */
     @DisplayName("Test partial hello server grpc route with Filibuster. (MyHelloService, MyWorldService)")
-    @FilibusterTest(dockerImageName = "filibustertesting/asdfasdf:0.33", degradeWhenServerInitializationFails = true)
+    @FilibusterTest(serverBackend = FilibusterDockerServerBackend.class, dockerImageName = "filibustertesting/asdfasdf:0.33", degradeWhenServerInitializationFails = true)
     @Order(1)
     public void testMyHelloAndMyWorldServiceWithFilibuster() throws InterruptedException {
         numberOfTestExecutions++;

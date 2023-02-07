13500a30b0308f0599bc349be50bdae975a6372d
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-06 19:16:33 -0500

    Refactor the report module.

--- src/main/java/cloud/filibuster/junit/server/core/test_executions/TestExecutionReport.java
+++ src/main/java/cloud/filibuster/junit/server/core/test_executions/TestExecutionReport.java
@@ -6,61 +6,49 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class TestExecutionReport {
-
     private final ArrayList<DistributedExecutionIndex> deiInvocationOrder = new ArrayList<>();
 
     private final HashMap<DistributedExecutionIndex, JSONObject> deiInvocations = new HashMap<>();
 
-    private final HashMap<DistributedExecutionIndex, JSONObject> deiInvocationRequests = new HashMap<>();
-
-    private final HashMap<DistributedExecutionIndex, JSONObject> deiInvocationResponses = new HashMap<>();
-
     private final HashMap<DistributedExecutionIndex, JSONObject> deiFaultsInjected = new HashMap<>();
 
-    public void recordInvocationAndRequest(
+    public void recordInvocation(
             DistributedExecutionIndex distributedExecutionIndex,
-            JSONObject invocationJsonObject,
-            JSONObject requestJsonObject
+            JSONObject invocationJsonObject
     ) {
         // Add to invocation order list.
         deiInvocationOrder.add(distributedExecutionIndex);
 
         // ...then, record the information about the invocation.
         deiInvocations.put(distributedExecutionIndex, invocationJsonObject);
-
-        // ...then, record the information about the request.
-        deiInvocationRequests.put(distributedExecutionIndex, requestJsonObject);
     }
 
-    public void recordInvocationResponse(
-            DistributedExecutionIndex distributedExecutionIndex,
-            JSONObject responseJsonObject
-    ) {
-        // Add to invocation response list.
-        deiInvocationResponses.put(distributedExecutionIndex, responseJsonObject);
+    public void setFaultsInjected(HashMap<DistributedExecutionIndex, JSONObject> faultsToInject) {
+        deiFaultsInjected.putAll(faultsToInject);
     }
 
-    public void setFaultsInjected() {
-        // TODO
+    static class Keys {
+        private static String DEI_KEY = "dei";
+        private static String DETAILS_KEY = "details";
+        private static String FAULT_KEY = "fault";
+        private static String RPCS_KEY = "rpcs";
+        private static String PASSED_KEY = "passed";
     }
 
-    // TODO: replace strings with constants.
     @SuppressWarnings("MemberName")
     public JSONObject toJSONObject() {
         ArrayList<JSONObject> RPCs = new ArrayList<>();
 
         for (DistributedExecutionIndex dei : deiInvocationOrder) {
             JSONObject RPC = new JSONObject();
-            RPC.put("dei", dei.toString());
-            RPC.put("details", deiInvocations.getOrDefault(dei, new JSONObject()));
-            RPC.put("request", deiInvocationRequests.getOrDefault(dei, new JSONObject()));
-            RPC.put("response", deiInvocationResponses.getOrDefault(dei, new JSONObject()));
-            RPC.put("fault", deiFaultsInjected.getOrDefault(dei, new JSONObject()));
+            RPC.put(Keys.DEI_KEY, dei.toString());
+            RPC.put(Keys.DETAILS_KEY, deiInvocations.getOrDefault(dei, new JSONObject()));
+            RPC.put(Keys.FAULT_KEY, deiFaultsInjected.getOrDefault(dei, new JSONObject()));
             RPCs.add(RPC);
         }
 
         JSONObject result = new JSONObject();
-        result.put("rpcs", RPCs);
+        result.put(Keys.RPCS_KEY, RPCs);
         return result;
     }
 }

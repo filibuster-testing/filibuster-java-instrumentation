35d915723184ee97732594f6bb0170a5b365f38c
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-13 03:06:34 -0500

    Cleanup warnings on LCS module.

--- src/main/java/cloud/filibuster/junit/server/core/lint/analyzers/LCS.java
+++ src/main/java/cloud/filibuster/junit/server/core/lint/analyzers/LCS.java
@@ -5,30 +5,28 @@ public class LCS {
 
     }
 
-    /** function lcs, from https://www.sanfoundry.com/java-program-longest-common-substring-algorithm/ **/
+    /** function lcs, from <a href="https://www.sanfoundry.com/java-program-longest-common-substring-algorithm/">...</a> **/
     public static String computeLCS(String str1, String str2)
     {
         int l1 = str1.length();
         int l2 = str2.length();
 
         int[][] arr = new int[l1 + 1][l2 + 1];
-        int len = 0, pos = -1;
+        int len = 0;
+        int pos = -1;
 
-        for (int x = 1; x < l1 + 1; x++)
-        {
-            for (int y = 1; y < l2 + 1; y++)
-            {
-                if (str1.charAt(x - 1) == str2.charAt(y - 1))
-                {
+        for (int x = 1; x < l1 + 1; x++) {
+            for (int y = 1; y < l2 + 1; y++) {
+                if (str1.charAt(x - 1) == str2.charAt(y - 1)) {
                     arr[x][y] = arr[x - 1][y - 1] + 1;
-                    if (arr[x][y] > len)
-                    {
+                    if (arr[x][y] > len) {
                         len = arr[x][y];
                         pos = x;
                     }
                 }
-                else
+                else {
                     arr[x][y] = 0;
+                }
             }
         }
 

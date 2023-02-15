b267abec70da1bb96d54e8a7f75f55be3796b446
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-15 14:04:18 -0500

    Update latency profile test.

--- src/test/java/cloud/filibuster/functional/java/latency/JUnitFilibusterTestWithLatencyProfile.java
+++ src/test/java/cloud/filibuster/functional/java/latency/JUnitFilibusterTestWithLatencyProfile.java
@@ -13,6 +13,7 @@ import io.grpc.StatusRuntimeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.concurrent.TimeUnit;
@@ -22,34 +23,48 @@ import static cloud.filibuster.junit.Assertions.wasFaultInjected;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-/**
- * Test simple annotation usage.
- */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JUnitFilibusterTestWithLatencyProfile extends JUnitBaseTest {
-    /**
-     * Inject faults between Hello and World using Filibuster and assert proper faults are injected.
-     *
-     * @throws InterruptedException if teardown of gRPC channel fails.
-     */
+    private static int numberOfTestsExecuted = 0;
+
+    private static int numberOfExceptions = 0;
+
     @DisplayName("Test partial hello server grpc route with Filibuster. (MyHelloService, MyWorldService)")
-    @FilibusterTest(latencyProfile=Filibuster1000msLatencyProfile.class, expected=FilibusterAllowedTimeExceededException.class)
+    @FilibusterTest(latencyProfile=Filibuster1000msLatencyProfile.class)
     @Order(1)
-    public void testMyHelloAndMyWorldServiceWithFilibuster() throws InterruptedException {
+    public void testMyHelloAndMyWorldServiceWithFilibuster() throws Throwable {
         ManagedChannel helloChannel = ManagedChannelBuilder
                 .forAddress(Networking.getHost("hello"), Networking.getPort("hello"))
                 .usePlaintext()
                 .build();
 
-        assertPassesWithinMsOrThrowsUnderFault(1, StatusRuntimeException.class, () -> {
-            HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(helloChannel);
-            Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName("Armerian").build();
-            Hello.HelloReply reply = blockingStub.partialHello(request);
-            assertEquals("Hello, Armerian World!!", reply.getMessage());
-            assertFalse(wasFaultInjected());
-        });
+        numberOfTestsExecuted++;
+
+        try {
+            assertPassesWithinMsOrThrowsUnderFault(1, StatusRuntimeException.class, () -> {
+                HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(helloChannel);
+                Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName("Armerian").build();
+                Hello.HelloReply reply = blockingStub.partialHello(request);
+                assertEquals("Hello, Armerian World!!", reply.getMessage());
+                assertFalse(wasFaultInjected());
+            });
+        } catch (FilibusterAllowedTimeExceededException e) {
+            numberOfExceptions++;
+        }
 
         helloChannel.shutdownNow();
         helloChannel.awaitTermination(1000, TimeUnit.SECONDS);
+    }
+
+    @Test
+    @Order(2)
+    public void testNumberOfTestsExecuted() {
+        assertEquals(5, numberOfTestsExecuted);
+    }
+
+    @Test
+    @Order(2)
+    public void testNumberOfExceptions() {
+        assertEquals(1, numberOfExceptions);
     }
 }

499161e390839911580df957cf98f120314f2b4f
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-15 20:36:47 -0500

    New aggregate report builder.

--- src/main/java/cloud/filibuster/junit/server/core/test_execution_reports/TestExecutionAggregateReport.java
+++ src/main/java/cloud/filibuster/junit/server/core/test_execution_reports/TestExecutionAggregateReport.java
@@ -3,7 +3,10 @@ package cloud.filibuster.junit.server.core.test_execution_reports;
 import cloud.filibuster.exceptions.filibuster.FilibusterTestReportWriterException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
@@ -34,26 +37,35 @@ public class TestExecutionAggregateReport {
         }
 
         try {
-            Files.write(indexPath, defaultHtmlContent.getBytes(Charset.defaultCharset()));
+            Path constructionGifPath = Paths.get(directory + "/construction.gif");
+            File constructionGifResourcePath = getFileFromResource("html/test_execution_aggregate_report/construction.gif");
+            Files.write(constructionGifPath, Files.readAllBytes(constructionGifResourcePath.toPath()));
         } catch (IOException e) {
             throw new FilibusterTestReportWriterException("Filibuster failed to write out the test execution aggregate report: ", e);
+        } catch (URISyntaxException e) {
+            throw new FilibusterTestReportWriterException("Filibuster failed to open resource file: ", e);
+        }
+
+        try {
+            File resourceIndexPath = getFileFromResource("html/test_execution_aggregate_report/waiting.html");
+            Files.write(indexPath, Files.readAllBytes(resourceIndexPath.toPath()));
+        } catch (IOException e) {
+            throw new FilibusterTestReportWriterException("Filibuster failed to write out the test execution aggregate report: ", e);
+        } catch (URISyntaxException e) {
+            throw new FilibusterTestReportWriterException("Filibuster failed to open resource file: ", e);
         }
     }
 
-    private static final String defaultHtmlContent = "<html lang=\"en\">\n" +
-            "\n" +
-            "<head>\n" +
-            "    <meta charset=\"UTF-8\">\n" +
-            "    <title>Filibuster Test Execution Report</title>\n" +
-            "</head>\n" +
-            "<body>\n" +
-            "\n" +
-            "    <div style=\"margin: 0 auto; width: 100px;\">\n" +
-            "        Please wait...\n" +
-            "    </div>\n" +
-            "\n" +
-            "</body>\n" +
-            "</html>";
+    private File getFileFromResource(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+
+        if (resource == null) {
+            throw new FilibusterTestReportWriterException("Filibuster failed to open resource file; this is possibly a file not found for file: " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+    }
 
     public void writeTestExecutionAggregateReport() {
         Path directory = Paths.get("/tmp/filibuster/");
@@ -67,7 +79,7 @@ public class TestExecutionAggregateReport {
         }
 
         // Write out the actual JSON data.
-        Path scriptFile = Paths.get(directory + "/analysis.js");
+        Path scriptFile = Paths.get(directory + "/summary.js");
         try {
             Files.write(scriptFile, toJavascript().getBytes(Charset.defaultCharset()));
         } catch (IOException e) {
@@ -77,9 +89,12 @@ public class TestExecutionAggregateReport {
         // Write out index file.
         Path indexPath = Paths.get(directory + "/index.html");
         try {
-            Files.write(indexPath, htmlContent.getBytes(Charset.defaultCharset()));
+            File resourceIndexPath = getFileFromResource("html/test_execution_aggregate_report/index.html");
+            Files.write(indexPath, Files.readAllBytes(resourceIndexPath.toPath()));
         } catch (IOException e) {
             throw new FilibusterTestReportWriterException("Filibuster failed to write out the test execution aggregate report: ", e);
+        } catch (URISyntaxException e) {
+            throw new FilibusterTestReportWriterException("Filibuster failed to open resource file: ", e);
         }
 
         logger.info(
@@ -105,120 +120,6 @@ public class TestExecutionAggregateReport {
 
     private String toJavascript() {
         JSONObject jsonObject = toJSONObject();
-        return "var analysis = " + jsonObject.toString(4) + ";";
+        return "var summary = " + jsonObject.toString(4) + ";";
     }
-
-    private static final String htmlContent = "<html lang=\"en\">\n" +
-            "\n" +
-            "<head>\n" +
-            "    <meta charset=\"UTF-8\">\n" +
-            "    <title>Filibuster Test Execution Report</title>\n" +
-            "    <script src=\"https://code.jquery.com/jquery-3.5.1.js\"></script>\n" +
-            "    <script type=\"text/javascript\" src=\"./analysis.js\"></script>\n" +
-            "    <style>\n" +
-            "\t\ttable {\n" +
-            "\t\t\tmargin: 0 auto;\n" +
-            "\t\t\tfont-size: large;\n" +
-            "\t\t\tborder: 1px solid black;\n" +
-            "            table-layout: fixed;\n" +
-            "\t\t}\n" +
-            "\n" +
-            "\t\th1 {\n" +
-            "\t\t\ttext-align: center;\n" +
-            "\t\t\tcolor: #006600;\n" +
-            "\t\t\tfont-size: xx-large;\n" +
-            "\t\t\tfont-family: 'Gill Sans',\n" +
-            "\t\t\t\t'Gill Sans MT', ' Calibri',\n" +
-            "\t\t\t\t'Trebuchet MS', 'sans-serif';\n" +
-            "\t\t}\n" +
-            "\n" +
-            "        div {\n" +
-            "\t\t\ttext-align: center;\n" +
-            "            margin: 0 auto;\n" +
-            "            width: 500px;\n" +
-            "            padding-bottom: 10px;\n" +
-            "        }\n" +
-            "\n" +
-            "        .fail { \n" +
-            "            font-weight: bold;\n" +
-            "            color: red;\n" +
-            "        }\n" +
-            "\n" +
-            "        .pass { \n" +
-            "            font-weight: bold;\n" +
-            "            color: green;\n" +
-            "        }\n" +
-            "\n" +
-            "\t\ttd {\n" +
-            "\t\t\tborder: 1px solid black;\n" +
-            "\t\t}\n" +
-            "\n" +
-            "\t\tth,\n" +
-            "\t\ttd {\n" +
-            "\t\t\tfont-weight: bold;\n" +
-            "\t\t\tborder: 1px solid black;\n" +
-            "\t\t\tpadding: 10px;\n" +
-            "\t\t\ttext-align: center;\n" +
-            "\t\t}\n" +
-            "\n" +
-            "\t\ttd {\n" +
-            "            font-weight: lighter;\n" +
-            "\t\t}\n" +
-            "\n" +
-            "        tr.success {\n" +
-            "            background-color: green;\n" +
-            "        }\n" +
-            "\n" +
-            "        tr.exception {\n" +
-            "            background-color: yellow;\n" +
-            "        }\n" +
-            "\n" +
-            "        tr.fault {\n" +
-            "            background-color: red;\n" +
-            "        }\n" +
-            "\t</style>\n" +
-            "</head>\n" +
-            "\n" +
-            "<body>\n" +
-            "<section>\n" +
-            "    <h1>Filibuster Test Execution Report</h1>\n" +
-            "\n" +
-            "    <div id='status'></div>\n" +
-            "\n" +
-            "    <table id='table'>\n" +
-            "        <tr>\n" +
-            "            <th>Test Execution</th>\n" +
-            "            <th>Status</th>\n" +
-            "            <th>Link</th>\n" +
-            "        </tr>\n" +
-            "\n" +
-            "        <script>\n" +
-            "\t\t\t\t$(document).ready(function () {\n" +
-            "                    console.log(analysis);\n" +
-            "\n" +
-            "                    for (i in analysis.reports) {\n" +
-            "                        var row = '';\n" +
-            "                        var report = analysis.reports[i];\n" +
-            "                        var statusText = '';\n" +
-            "\n" +
-            "                        if (report.status == true) {\n" +
-            "                            statusText = '<font style=\"color: green\">Passed</font>';\n" +
-            "                        } else {\n" +
-            "                            statusText = '<font style=\"color: red\">Failed</font>';\n" +
-            "                        }\n" +
-            "\n" +
-            "                        row += '<tr>';\n" +
-            "                        row += '<td>' + report.iteration + '</td>';\n" +
-            "                        row += '<td>' + statusText + '</td>';\n" +
-            "                        row += '<td><a href=\"' + report.path + '\">Link To Report</a></td>';\n" +
-            "                        row += '</tr>';\n" +
-            "\n" +
-            "\t\t\t\t\t\t$('#table').append(row);\n" +
-            "                    }\n" +
-            "\t\t\t\t});\n" +
-            "\t\t\t</script>\n" +
-            "</section>\n" +
-            "</body>\n" +
-            "\n" +
-            "</html>\n";
 }

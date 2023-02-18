574c3acaaff49dd38c0aa287dad2b35a687fa047
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-17 19:03:31 -0500

    Add new warning type.

---
+++ src/main/java/cloud/filibuster/junit/server/core/lint/analyzers/test_execution_report/IncompleteRPCAnalyzer.java
@@ -0,0 +1,33 @@
+package cloud.filibuster.junit.server.core.lint.analyzers.test_execution_report;
+
+import cloud.filibuster.dei.DistributedExecutionIndex;
+import cloud.filibuster.exceptions.filibuster.FilibusterAnalysisFailureException;
+import cloud.filibuster.junit.server.core.lint.analyzers.warnings.IncompleteRPCWarning;
+import cloud.filibuster.junit.server.core.lint.analyzers.warnings.RedundantRPCWarning;
+import cloud.filibuster.junit.server.core.test_execution_reports.TestExecutionReport;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IncompleteRPCAnalyzer extends TestExecutionReportAnalyzer {
+    public IncompleteRPCAnalyzer(TestExecutionReport testExecutionReport) {
+        super(testExecutionReport);
+    }
+
+    private final List<String> seenRPCs = new ArrayList<>();
+
+    @Override
+    void rpc(int RPC, DistributedExecutionIndex distributedExecutionIndex, JSONObject invocation, JSONObject response) {
+        String method = invocation.getString("method");
+
+        if (response == null) {
+            this.addWarning(new IncompleteRPCWarning(distributedExecutionIndex, method));
+        }
+    }
+
+    @Override
+    boolean shouldReportErrorBasedOnTestStatus(boolean testPassed) {
+        return true;
+    }
+}

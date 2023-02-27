4f2ead23d691095c7bcc30b9590c08b412e7ff72
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-27 01:48:09 -0500

    Add API service definition.

---
+++ src/test/java/cloud/filibuster/integration/examples/armeria/grpc/test_services/MyAPIService.java
@@ -0,0 +1,91 @@
+package cloud.filibuster.integration.examples.armeria.grpc.test_services;
+
+import cloud.filibuster.examples.APIServiceGrpc;
+import cloud.filibuster.examples.Hello;
+import cloud.filibuster.examples.HelloServiceGrpc;
+import cloud.filibuster.exceptions.CircuitBreakerException;
+import cloud.filibuster.instrumentation.helpers.Networking;
+import cloud.filibuster.instrumentation.libraries.grpc.FilibusterClientInterceptor;
+import cloud.filibuster.integration.instrumentation.libraries.opentelemetry.OpenTelemetryFilibusterClientInterceptor;
+import io.grpc.Channel;
+import io.grpc.ClientInterceptor;
+import io.grpc.ClientInterceptors;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyAPIService extends APIServiceGrpc.APIServiceImplBase {
+    private static final Logger logger = Logger.getLogger(MyAPIService.class.getName());
+
+    public static boolean shouldReturnRuntimeExceptionWithDescription = false;
+    public static boolean shouldReturnRuntimeExceptionWithCause = false;
+    public static boolean shouldReturnExceptionWithDescription = false;
+    public static boolean shouldReturnExceptionWithCause = false;
+
+    public static boolean useOtelClientInterceptor = false;
+    public static boolean shouldUseDecorator = false;
+
+    @Override
+    public void hello(Hello.HelloRequest req, StreamObserver<Hello.HelloReply> responseObserver) {
+        // build stub with decorator or interceptor
+        ManagedChannel originalChannel = ManagedChannelBuilder
+                .forAddress(Networking.getHost("hello"), Networking.getPort("hello"))
+                .usePlaintext()
+                .build();
+
+        ClientInterceptor clientInterceptor;
+
+        if (useOtelClientInterceptor) {
+            clientInterceptor = new OpenTelemetryFilibusterClientInterceptor("api_server", null, null);
+        } else {
+            clientInterceptor = new FilibusterClientInterceptor("api_server");
+        }
+
+        Channel channel = ClientInterceptors.intercept(originalChannel, clientInterceptor);
+
+        try {
+            HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel);
+            Hello.HelloExtendedRequest request = Hello.HelloExtendedRequest.newBuilder().setName(req.getName()).build();
+            Hello.HelloExtendedReply helloExtendedReply = blockingStub.compositionalHello(request);
+
+            Hello.HelloReply reply = Hello.HelloReply.newBuilder()
+                    .setMessage("Hello, " + helloExtendedReply.getFirstMessage())
+                    .build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+
+            originalChannel.shutdownNow();
+            try {
+                while (! originalChannel.awaitTermination(1000, TimeUnit.SECONDS)) {
+                    Thread.sleep(4000);
+                }
+            } catch (InterruptedException ie) {
+                logger.log(Level.SEVERE, "Failed to terminate channel: " + ie);
+            }
+
+        } catch (StatusRuntimeException e) {
+            if (e.getCause() instanceof CircuitBreakerException) {
+                Status status = Status.INTERNAL.withDescription(e.toString());
+                responseObserver.onError(status.asRuntimeException());
+            } else {
+                Status status = Status.FAILED_PRECONDITION.withDescription(e.toString());
+                responseObserver.onError(status.asRuntimeException());
+            }
+
+            originalChannel.shutdownNow();
+            try {
+                while (! originalChannel.awaitTermination(1000, TimeUnit.SECONDS)) {
+                    Thread.sleep(4000);
+                }
+            } catch (InterruptedException ie) {
+                logger.log(Level.SEVERE, "Failed to terminate channel: " + ie);
+            }
+        }
+    }
+}

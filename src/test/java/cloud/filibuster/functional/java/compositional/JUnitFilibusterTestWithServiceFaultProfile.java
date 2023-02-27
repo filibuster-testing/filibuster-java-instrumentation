5757b65cae1f3f23cc7a120656c1f82c19a663c2
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-02-27 17:25:49 -0500

    Add new test.

--- src/test/java/cloud/filibuster/functional/java/compositional/JUnitFilibusterTestWithServiceFaultProfile.java
+++ src/test/java/cloud/filibuster/functional/java/compositional/JUnitFilibusterTestWithServiceFaultProfile.java
@@ -8,12 +8,14 @@ import cloud.filibuster.instrumentation.helpers.Networking;
 import cloud.filibuster.junit.FilibusterTest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.grpcmock.junit5.GrpcMockExtension;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -25,24 +27,27 @@ import static cloud.filibuster.integration.instrumentation.TestHelper.stopAPISer
 import static cloud.filibuster.junit.Assertions.wasFaultInjected;
 import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnMethod;
 import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnService;
+import static org.grpcmock.GrpcMock.stubFor;
+import static org.grpcmock.GrpcMock.unaryMethod;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Test simple annotation usage.
- */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class JUnitFilibusterTest extends JUnitBaseTest {
+public class JUnitFilibusterTestWithServiceFaultProfile extends JUnitBaseTest {
+
+    @RegisterExtension
+    static GrpcMockExtension grpcMockExtension = GrpcMockExtension.builder()
+            .withPort(Networking.getPort("hello-mock"))
+            .build();
 
     private final static Set<String> testExceptionsThrownForHelloService = new HashSet<>();
 
     private final static Set<String> testExceptionsThrownForAPIService = new HashSet<>();
 
-    private final static Set<String> testExceptionsThrownForAPIServiceWithServiceFaultProfiles = new HashSet<>();
-
+    private final static Set<String> testExceptionsThrownForAPIServiceWithMock = new HashSet<>();
 
     @BeforeAll
     public static void startAPIServer() throws IOException, InterruptedException {
@@ -212,86 +217,83 @@ public class JUnitFilibusterTest extends JUnitBaseTest {
         assertEqualsUnlessFilibusterDisabledByEnvironment(8, testExceptionsThrownForAPIService.size());
     }
 
-    // Test 5, use the service fault profile for API service.
-//    @FilibusterTest(dataNondeterminism = true, serviceFaultProfiles = "/tmp/filibuster/service")
-//    @Order(5)
-//    public void testAPIServiceWithServiceFaultProfiles() throws InterruptedException {
-//        ManagedChannel apiChannel = ManagedChannelBuilder
-//                .forAddress(Networking.getHost("api_server"), Networking.getPort("api_server"))
-//                .usePlaintext()
-//                .build();
-//
-//        boolean expected = false;
-//
-//        try {
-//            String name = "API";
-//
-//            APIServiceGrpc.APIServiceBlockingStub blockingStub = APIServiceGrpc.newBlockingStub(apiChannel);
-//            Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName(name).build();
-//            Hello.HelloReply reply = blockingStub.hello(request);
-//            assertEquals("Hello, Hello, " + name + " World!!", reply.getMessage());
-//            assertFalse(wasFaultInjected());
-//        } catch (Throwable t) {
-//            boolean wasFaultInjected = wasFaultInjected();
-//
-//            if (wasFaultInjected) {
-//                testExceptionsThrownForAPIServiceWithServiceFaultProfiles.add(t.getMessage());
-//
-//                // World failures.
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: DATA_LOSS: io.grpc.StatusRuntimeException: DEADLINE_EXCEEDED")) {
-//                    expected = true;
-//                }
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: DATA_LOSS: io.grpc.StatusRuntimeException: UNAVAILABLE")) {
-//                    expected = true;
-//                }
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: DATA_LOSS: io.grpc.StatusRuntimeException: INTERNAL")) {
-//                    expected = true;
-//                }
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: DATA_LOSS: io.grpc.StatusRuntimeException: UNIMPLEMENTED")) {
-//                    expected = true;
-//                }
-//
-//                // Hello failures.
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: DEADLINE_EXCEEDED")) {
-//                    expected = true;
-//                }
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: UNAVAILABLE")) {
-//                    expected = true;
-//                }
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: INTERNAL")) {
-//                    expected = true;
-//                }
-//
-//                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: UNIMPLEMENTED")) {
-//                    expected = true;
-//                }
-//
-//                // Otherwise.
-//
-//                if (! expected) {
-//                    throw t;
-//                }
-//            } else {
-//                throw t;
-//            }
-//        }
-//
-//        apiChannel.shutdownNow();
-//        apiChannel.awaitTermination(1000, TimeUnit.SECONDS);
-//    }
+    // Test 5, test the API service using a mock.
+    // - It should test fewer failures then when not using the mock.
+    @FilibusterTest(dataNondeterminism = true)
+    @Order(5)
+    public void testAPIServiceWithMock() throws InterruptedException {
+        ManagedChannel apiChannel = ManagedChannelBuilder
+                .forAddress(Networking.getHost("api_server"), Networking.getPort("api_server"))
+                .usePlaintext()
+                .build();
 
-//    @Test
-//    @Order(6)
-//    public void testNumAssertionsForAPIServiceWithServiceFaultProfiles() {
-//        assertEqualsUnlessFilibusterDisabledByEnvironment(8, testExceptionsThrownForAPIServiceWithServiceFaultProfiles.size());
-//    }
+        boolean expected = false;
 
-    // TODO: other tests for the other profiles.
+        String name = "API";
+
+        Hello.HelloExtendedReply response1 = Hello.HelloExtendedReply.newBuilder()
+                .setName(name)
+                .setFirstMessage("Hello, " + name + " World!!")
+                .setSecondMessage(String.valueOf(Math.random()))
+                .setCreatedAt("2023-01-01 01:02:03")
+                .build();
+
+        stubFor(unaryMethod(HelloServiceGrpc.getCompositionalHelloMethod()).willReturn(response1));
+
+        try {
+            APIServiceGrpc.APIServiceBlockingStub blockingStub = APIServiceGrpc.newBlockingStub(apiChannel);
+            Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName(name).build();
+            Hello.HelloReply reply = blockingStub.helloWithMock(request);
+            assertEquals("Hello, Hello, " + name + " World!!", reply.getMessage());
+            assertFalse(wasFaultInjected());
+        } catch (Throwable t) {
+            boolean wasFaultInjected = wasFaultInjected();
+
+            if (wasFaultInjected) {
+                testExceptionsThrownForAPIServiceWithMock.add(t.getMessage());
+
+                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: DEADLINE_EXCEEDED")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: UNAVAILABLE")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: INTERNAL")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("FAILED_PRECONDITION: io.grpc.StatusRuntimeException: UNIMPLEMENTED")) {
+                    expected = true;
+                }
+
+                if (! expected) {
+                    throw t;
+                }
+            } else {
+                throw t;
+            }
+        }
+
+        apiChannel.shutdownNow();
+        apiChannel.awaitTermination(1000, TimeUnit.SECONDS);
+    }
+
+    // Test 6
+    // - Verify Filibuster generated the right number of tests for API Service using a mock only.
+    @Test
+    @Order(6)
+    public void testNumAssertionsForAPIServiceWithMock() {
+        assertEqualsUnlessFilibusterDisabledByEnvironment(4, testExceptionsThrownForAPIServiceWithMock.size());
+    }
+    
+    // Test 7, test the API service using a mock and profile.
+    // - It should test the correct number of failures (original, full graph, more than mock alone.)
+    // TODO
+
+    // Test 8
+    // - Verify Filibuster generated the right number of tests for API Service using a mock and profile.
+    // TODO
+
 }

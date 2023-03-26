d332518d0492cd2fdb44f6aada949b078f67d622
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-03-26 01:56:42 -0400

    Further refactor.

---
+++ src/test/java/cloud/filibuster/functional/java/basic/JUnitFilibusterTest.java
@@ -0,0 +1,111 @@
+package cloud.filibuster.functional.java.basic;
+
+import cloud.filibuster.examples.Hello;
+import cloud.filibuster.examples.HelloServiceGrpc;
+import cloud.filibuster.functional.java.JUnitAnnotationBaseTest;
+import cloud.filibuster.instrumentation.helpers.Networking;
+import cloud.filibuster.junit.FilibusterConditionalByEnvironmentSuite;
+import cloud.filibuster.junit.FilibusterTest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static cloud.filibuster.junit.Assertions.wasFaultInjected;
+import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnMethod;
+import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Test simple annotation usage.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@FilibusterConditionalByEnvironmentSuite
+public class JUnitFilibusterTest extends JUnitAnnotationBaseTest {
+    private final static Set<String> testExceptionsThrown = new HashSet<>();
+
+    /**
+     * Inject faults between Hello and World using Filibuster and assert proper faults are injected.
+     *
+     * @throws InterruptedException if teardown of gRPC channel fails.
+     */
+    @DisplayName("Test partial hello server grpc route with Filibuster. (MyHelloService, MyWorldService)")
+    @FilibusterTest()
+    @Order(1)
+    public void testMyHelloAndMyWorldServiceWithFilibuster() throws InterruptedException {
+        ManagedChannel helloChannel = ManagedChannelBuilder
+                .forAddress(Networking.getHost("hello"), Networking.getPort("hello"))
+                .usePlaintext()
+                .build();
+
+        boolean expected = false;
+
+        try {
+            HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(helloChannel);
+            Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName("Armerian").build();
+            Hello.HelloReply reply = blockingStub.partialHello(request);
+            assertEquals("Hello, Armerian World!!", reply.getMessage());
+            assertFalse(wasFaultInjected());
+        } catch (Throwable t) {
+            boolean wasFaultInjected = wasFaultInjected();
+
+            if (wasFaultInjected) {
+                testExceptionsThrown.add(t.getMessage());
+
+                if (t.getMessage().equals("DATA_LOSS: io.grpc.StatusRuntimeException: DEADLINE_EXCEEDED")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("DATA_LOSS: io.grpc.StatusRuntimeException: UNAVAILABLE")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("DATA_LOSS: io.grpc.StatusRuntimeException: UNIMPLEMENTED")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("DATA_LOSS: io.grpc.StatusRuntimeException: INTERNAL")) {
+                    expected = true;
+                }
+
+                if (t.getMessage().equals("DATA_LOSS: io.grpc.StatusRuntimeException: UNKNOWN")) {
+                    expected = true;
+                }
+
+                boolean wasFaultInjectedOnWorldService = wasFaultInjectedOnService("WorldService");
+                assertTrue(wasFaultInjectedOnWorldService);
+
+                boolean wasFaultInjectedOnWorldMethod = wasFaultInjectedOnMethod("cloud.filibuster.examples.WorldService/World");
+                assertTrue(wasFaultInjectedOnWorldMethod);
+
+                if (! expected) {
+                    throw t;
+                }
+            } else {
+                throw t;
+            }
+        }
+
+        helloChannel.shutdownNow();
+        helloChannel.awaitTermination(1000, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Verify that Filibuster generated the correct number of fault injections.
+     */
+    @DisplayName("Verify correct number of generated Filibuster tests.")
+    @Test
+    @Order(2)
+    public void testNumAssertions() {
+        assertEquals(5, testExceptionsThrown.size());
+    }
+}
---
+++ src/test/java/cloud/filibuster/functional/java/latency/JUnitFilibusterTestWithLatencyInjectionWithFailure.java
@@ -0,0 +1,66 @@
+package cloud.filibuster.functional.java.latency;
+
+import cloud.filibuster.examples.Hello;
+import cloud.filibuster.examples.HelloServiceGrpc;
+import cloud.filibuster.exceptions.filibuster.FilibusterAllowedTimeExceededException;
+import cloud.filibuster.functional.java.JUnitAnnotationBaseTest;
+import cloud.filibuster.instrumentation.helpers.Networking;
+import cloud.filibuster.junit.FilibusterTest;
+import cloud.filibuster.junit.configuration.examples.FilibusterLatencyOnlyAnalysisConfigurationFile;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.concurrent.TimeUnit;
+
+import static cloud.filibuster.junit.Assertions.assertPassesWithinMs;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class JUnitFilibusterTestWithLatencyInjectionWithFailure extends JUnitAnnotationBaseTest {
+    private static int numberOfTestsExecuted = 0;
+
+    private static int numberOfExceptions = 0;
+
+    @DisplayName("Test partial hello server grpc route with Filibuster. (MyHelloService, MyWorldService)")
+    @FilibusterTest(analysisConfigurationFile=FilibusterLatencyOnlyAnalysisConfigurationFile.class)
+    @Order(1)
+    public void testMyHelloAndMyWorldServiceWithFilibuster() throws Throwable {
+        ManagedChannel helloChannel = ManagedChannelBuilder
+                .forAddress(Networking.getHost("hello"), Networking.getPort("hello"))
+                .usePlaintext()
+                .build();
+
+        numberOfTestsExecuted++;
+
+        try {
+            assertPassesWithinMs(1, () -> {
+                HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(helloChannel);
+                Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName("Armerian").build();
+                Hello.HelloReply reply = blockingStub.partialHello(request);
+                assertEquals("Hello, Armerian World!!", reply.getMessage());
+            });
+        } catch (FilibusterAllowedTimeExceededException e) {
+            numberOfExceptions++;
+        }
+
+        helloChannel.shutdownNow();
+        helloChannel.awaitTermination(1000, TimeUnit.SECONDS);
+    }
+
+    @Test
+    @Order(2)
+    public void testNumberOfTestsExecuted() {
+        assertEquals(2, numberOfTestsExecuted);
+    }
+
+    @Test
+    @Order(2)
+    public void testNumberOfExceptions() {
+        assertEquals(2, numberOfExceptions);
+    }
+}

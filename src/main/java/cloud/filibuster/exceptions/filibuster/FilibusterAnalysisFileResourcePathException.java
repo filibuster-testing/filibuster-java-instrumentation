09a7ea61e9614f2c7b90f7b5099cc7608abf5fae
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-04-20 21:38:31 -0400

    Avoid NPE.

---
+++ src/main/java/cloud/filibuster/exceptions/filibuster/FilibusterAnalysisFileResourcePathException.java
@@ -0,0 +1,15 @@
+package cloud.filibuster.exceptions.filibuster;
+
+public class FilibusterAnalysisFileResourcePathException extends FilibusterRuntimeException {
+    public FilibusterAnalysisFileResourcePathException(String message) {
+        super(message);
+    }
+
+    public FilibusterAnalysisFileResourcePathException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FilibusterAnalysisFileResourcePathException(Throwable cause) {
+        super(cause);
+    }
+}

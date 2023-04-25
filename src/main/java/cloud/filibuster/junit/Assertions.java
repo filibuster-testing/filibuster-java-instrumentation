2f8a04a035c6372ffbc42016a797976b5d69cd4f
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2023-04-24 23:28:31 -0400

    Change interface to RuntimeException instead of Throwable.

--- src/main/java/cloud/filibuster/junit/Assertions.java
+++ src/main/java/cloud/filibuster/junit/Assertions.java
@@ -104,13 +104,24 @@ public class Assertions {
      * @param testBlock block containing the test code to execute.
      * @param thrownAssertionBlock block containing the conditional assertions to execute (throws, takes one parameter containing a @Throwable.)
      */
-    public static void assertPassesAndThrowsOnlyUnderFault(Runnable testBlock, ThrowingConsumer<Throwable> thrownAssertionBlock) throws Throwable {
+    public static void assertPassesAndThrowsOnlyUnderFault(Runnable testBlock, ThrowingConsumer<Throwable> thrownAssertionBlock) throws RuntimeException {
         try {
             testBlock.run();
         } catch (Throwable t) {
             if (wasFaultInjected()) {
                 // Test threw, we expected it: now check the conditional, user-provided, assertions.
-                thrownAssertionBlock.accept(t);
+                try {
+                    thrownAssertionBlock.accept(t);
+                } catch (Throwable t1) {
+                    // We should only see RuntimeExceptions, but if we don't for some bizarre reason, wrap them in a RuntimeException and re-throw.
+                    throw new FilibusterRuntimeException(t1);
+                }
+            } else {
+                // Test threw, we didn't inject a fault: throw.
+                throw t;
+            }
+        }
+    }
             } else {
                 // Test threw, we didn't inject a fault: throw.
                 throw t;

86dc7f4002698a539a5e7ab65d429ba1af2108db
Michael A <michael.assad@tum.de>
2023-04-27 23:51:33 -0400

    Remove DefaultMethodInvokingInterceptor

--- src/main/java/cloud/filibuster/instrumentation/libraries/lettuce/LettuceInterceptedConnection.java
+++ src/main/java/cloud/filibuster/instrumentation/libraries/lettuce/LettuceInterceptedConnection.java
@@ -1,7 +1,6 @@
 package cloud.filibuster.instrumentation.libraries.lettuce;
 
 import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.dynamic.intercept.DefaultMethodInvokingInterceptor;
 import io.lettuce.core.dynamic.intercept.InvocationProxyFactory;
 
 public abstract class LettuceInterceptedConnection {
@@ -9,7 +8,6 @@ public abstract class LettuceInterceptedConnection {
     public static MyRedisCommands create (StatefulRedisConnection<String, String> redisConnection) {
         InvocationProxyFactory myFactory = new InvocationProxyFactory();
         myFactory.addInterface(MyRedisCommands.class);
-        myFactory.addInterceptor(new DefaultMethodInvokingInterceptor());
         myFactory.addInterceptor(new LettuceInterceptor(redisConnection));
         myFactory.addInterceptor(new LettuceInterceptorExecutor(redisConnection));
         return myFactory.createProxy(MyRedisCommands.class.getClassLoader());

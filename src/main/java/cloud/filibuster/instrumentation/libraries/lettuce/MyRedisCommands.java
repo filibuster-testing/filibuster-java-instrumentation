00d5eaca7f2cc6767f9ba41f9dccf6680ec5013c
Michael A <michael.assad@tum.de>
2023-04-27 23:41:35 -0400

    Remove comments from MyRedisCommands interface

--- src/main/java/cloud/filibuster/instrumentation/libraries/lettuce/MyRedisCommands.java
+++ src/main/java/cloud/filibuster/instrumentation/libraries/lettuce/MyRedisCommands.java
@@ -3,6 +3,6 @@ package cloud.filibuster.instrumentation.libraries.lettuce;
 import io.lettuce.core.dynamic.Commands;
 
 public interface MyRedisCommands extends Commands {
-    String get(String key); // Synchronous Execution of GET
-    String set(String key, String value); // asynchronous SET execution
+    String get(String key);
+    String set(String key, String value);
 }

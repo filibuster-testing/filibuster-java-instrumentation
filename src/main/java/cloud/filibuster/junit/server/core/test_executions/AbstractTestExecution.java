fadd1874da565e623fd5091247d772bd72d81001
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-05-11 23:49:58 -0400

    Track a reference to the completed concrete execution this was cut from.

--- src/main/java/cloud/filibuster/junit/server/core/test_executions/AbstractTestExecution.java
+++ src/main/java/cloud/filibuster/junit/server/core/test_executions/AbstractTestExecution.java
@@ -4,17 +4,27 @@ import cloud.filibuster.dei.DistributedExecutionIndex;
 import org.json.JSONObject;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("Varifier")
 public class AbstractTestExecution extends TestExecution {
+    // This is a partial execution that gets us to this invocation point where we want to inject a fault.
     @Nullable
     private ConcreteTestExecution sourceConcreteTestExecution;
+
+    // This is a reference to the completed execution that this execution was generated from.
+    // This will reference RPCs that this abstract execution does not know about -- if they happened after the this
+    // abstract execution was cut.
+    @Nullable
+    private ConcreteTestExecution completedSourceConcreteTestExecution; 
 
     public AbstractTestExecution() {
     }
 
     public AbstractTestExecution(ConcreteTestExecution concreteTestExecution) {
         this.sourceConcreteTestExecution = (ConcreteTestExecution) concreteTestExecution.clone();
+        this.completedSourceConcreteTestExecution = concreteTestExecution;
     }
 
     public boolean sawInConcreteTestExecution(DistributedExecutionIndex distributedExecutionIndex) {

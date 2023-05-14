ac4d6382307c754f2240fea52bbb1f9f1f70e706
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-05-13 21:35:53 -0400

    Add test to exercise failure after/before.

---
+++ src/test/java/cloud/filibuster/functional/java/basic/FailingInSecondExecutionTest.java
@@ -0,0 +1,67 @@
+package cloud.filibuster.functional.java.basic;
+
+import cloud.filibuster.examples.Hello;
+import cloud.filibuster.examples.HelloServiceGrpc;
+import cloud.filibuster.functional.java.JUnitAnnotationBaseTest;
+import cloud.filibuster.instrumentation.helpers.Networking;
+import cloud.filibuster.junit.TestWithFilibuster;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static cloud.filibuster.junit.Assertions.wasFaultInjected;
+import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnMethod;
+import static cloud.filibuster.junit.Assertions.wasFaultInjectedOnService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FailingInSecondExecutionTest extends JUnitAnnotationBaseTest {
+    private static int invocationCount = 0;
+
+    @BeforeEach
+    public void doNothingBefore() {
+        // Nothing, just make sure we call this block.
+    }
+
+    @AfterEach
+    public void doNothingAfter() {
+        // Nothing, just make sure we call this block.
+    }
+
+    @TestWithFilibuster()
+    @Order(1)
+    public void testMyHelloAndMyWorldServiceWithFilibuster() throws InterruptedException {
+        invocationCount++;
+
+        ManagedChannel helloChannel = ManagedChannelBuilder
+                .forAddress(Networking.getHost("hello"), Networking.getPort("hello"))
+                .usePlaintext()
+                .build();
+
+        try {
+            HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(helloChannel);
+            Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName("Armerian").build();
+            blockingStub.partialHello(request);
+        } catch (Throwable t) {
+            // Do nothing.
+        }
+
+        helloChannel.shutdownNow();
+        helloChannel.awaitTermination(1000, TimeUnit.SECONDS);
+
+        if (invocationCount == 2) {
+            fail();
+        }
+    }
+}

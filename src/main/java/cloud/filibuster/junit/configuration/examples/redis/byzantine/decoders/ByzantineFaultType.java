922a88c933af5850a361d692acdb79f4d1f98472
Michael Assad <michael.assad@tum.de>
2023-05-23 16:11:33 -0400

    Add comments to ByzantineFaultType.java

--- src/main/java/cloud/filibuster/junit/configuration/examples/redis/byzantine/decoders/ByzantineFaultType.java
+++ src/main/java/cloud/filibuster/junit/configuration/examples/redis/byzantine/decoders/ByzantineFaultType.java
@@ -3,8 +3,14 @@ package cloud.filibuster.junit.configuration.examples.redis.byzantine.decoders;
 import cloud.filibuster.exceptions.filibuster.FilibusterRuntimeException;
 
 public abstract class ByzantineFaultType<T> {
+
+    // Cast byzantineFaultValue to the target type T
     abstract public T cast(Object byzantineFaultValue);
+
+    // Return the fault type as ByzantineFault enum
     abstract ByzantineFault getFaultType();
+
+    // Get ByzantineFaultType object from a string representing the fault type encoded in the enum ByzantineFault
     public static ByzantineFaultType<?> fromFaultType(String stringFaultType) {
         ByzantineFault faultType = ByzantineFault.valueOf(stringFaultType);
         switch (faultType) {
@@ -15,6 +21,7 @@ public abstract class ByzantineFaultType<T> {
         }
         throw new FilibusterRuntimeException("Unknown ByzantineFaultType: " + faultType);
     }
+
     @Override
     public String toString() {
         return getFaultType().toString();

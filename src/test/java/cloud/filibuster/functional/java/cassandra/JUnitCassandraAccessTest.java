2655adf0ca87baf2aad630253c9377fd3edbdaca
Michael Assad <michael.assad@tum.de>
2023-06-05 19:50:28 -0400

    Implement Cassandra service and test it

---
+++ src/test/java/cloud/filibuster/functional/java/cassandra/JUnitCassandraAccessTest.java
@@ -0,0 +1,34 @@
+package cloud.filibuster.functional.java.cassandra;
+
+import cloud.filibuster.functional.java.JUnitAnnotationBaseTest;
+import cloud.filibuster.integration.examples.armeria.grpc.test_services.CassandraClientService;
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.metadata.schema.KeyspaceMetadata;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class JUnitCassandraAccessTest extends JUnitAnnotationBaseTest {
+
+    @Test
+    @DisplayName("Tests whether CockroachDB/PostgreSQL connection can read and write")
+    @Order(1)
+    public void testPostgresAndCockroachAccess() {
+        CqlSession cassandraSession = CassandraClientService.getInstance().cassandraClient;
+
+        cassandraSession.execute(
+                "CREATE KEYSPACE IF NOT EXISTS test WITH replication = \n" +
+                        "{'class':'SimpleStrategy','replication_factor':'1'};"
+        );
+
+        KeyspaceMetadata keyspace = cassandraSession.getMetadata().getKeyspaces().get(CqlIdentifier.fromCql("test"));
+
+        assertNotNull(keyspace, "Keyspace was not created");
+    }
+}

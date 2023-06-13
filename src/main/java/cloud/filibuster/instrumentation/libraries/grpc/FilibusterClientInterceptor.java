558411ca9823867292a760bc861ea864890e28eb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-06-13 00:23:31 -0400

    Call setRpcType from GRPC interceptors.

--- src/main/java/cloud/filibuster/instrumentation/libraries/grpc/FilibusterClientInterceptor.java
+++ src/main/java/cloud/filibuster/instrumentation/libraries/grpc/FilibusterClientInterceptor.java
@@ -278,6 +278,7 @@ public class FilibusterClientInterceptor implements ClientInterceptor {
                     // Record invocation.
                     // ******************************************************************************************
 
+                    filibusterClientInstrumentor.setRpcType("grpc"); // TODO: enum?
                     filibusterClientInstrumentor.beforeInvocation();
 
                     // ******************************************************************************************

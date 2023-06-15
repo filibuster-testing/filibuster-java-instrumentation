5c4ee793e465b04ffb4183ef9f96d18bdb1bc53d
Michael Assad <michael.assad@tum.de>
2023-06-15 01:17:30 -0400

    Rename transformerByzantine to transformer

--- src/main/java/cloud/filibuster/instrumentation/instrumentors/FilibusterClientInstrumentor.java
+++ src/main/java/cloud/filibuster/instrumentation/instrumentors/FilibusterClientInstrumentor.java
@@ -459,6 +459,7 @@ final public class FilibusterClientInstrumentor {
      * Return transformer fault that needs to be injected.
      * This value will be null until the Filibuster server has been contacted for this request.
      *
+
      * @return JSON object containing failure to inject.
      */
     public JSONObject getTransformerFault() {

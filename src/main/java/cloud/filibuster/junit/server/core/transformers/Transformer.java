c6acc75b7e6b562293ad17deb01792034b3e7549
Michael Assad <michael.assad@tum.de>
2023-06-15 15:08:26 -0400

    Add summary lines to ITransformer javadoc

--- src/main/java/cloud/filibuster/junit/server/core/transformers/Transformer.java
+++ src/main/java/cloud/filibuster/junit/server/core/transformers/Transformer.java
@@ -1,51 +1,69 @@
 package cloud.filibuster.junit.server.core.transformers;
 
 /**
+ * Returns a generic transformer interface that can be implemented in type-specific classes (e.g., StringTransformer).
+ *
  * @param <PAYLOAD> Type of the object to be transformed
  * @param <CONTEXT> Type used in the accumulator to identify current iteration (e.g., Integer representing idx
  *                  of mutated char in a string)
  */
 public interface Transformer<PAYLOAD, CONTEXT> {
     /**
+     * Returns transformer instance containing the payload after applying the transformation.
+     *
      * @param payload     Value to be transformed
      * @param accumulator Accumulator of current iteration
-     * @return Transformer instance containing the payload after applying the transformation
+     * @return Transformer
      */
     Transformer<PAYLOAD, CONTEXT> transform(PAYLOAD payload, Accumulator<PAYLOAD, CONTEXT> accumulator);
 
     /**
-     * @return Whether current iteration has a successor
+     * Returns whether current iteration has a successor.
+     *
+     * @return boolean
      */
     boolean hasNext();
 
     /**
+     * Returns class of the payload.
+     *
      * @return Class of the payload
      */
     Class<PAYLOAD> getPayloadType();
 
     /**
+     * Returns class of the context.
+     *
      * @return Class of the context
      */
     Class<CONTEXT> getContextType();
 
     /**
-     * @return Payload after applying the transformation.
+     * Returns payload after applying the transformation.
      * The transform method should be called at least once before this method.
+     *
+     * @return Payload
      */
     PAYLOAD getResult();
 
     /**
-     * @return Accumulator used in current transformation
+     * Returns accumulator used in current transformation.
+     *
+     * @return Generic accumulator
      */
     Accumulator<PAYLOAD, CONTEXT> getAccumulator();
 
     /**
-     * @return Initial accumulator used in the first transformation
+     * Returns initial accumulator used in the first transformation.
+     *
+     * @return Initial accumulator
      */
     Accumulator<PAYLOAD, CONTEXT> getInitialAccumulator();
 
     /**
-     * @return Accumulator of next transformation
+     * Returns accumulator of next transformation.
+     *
+     * @return Generic accumulator
      */
     Accumulator<PAYLOAD, CONTEXT> getNextAccumulator();
 }

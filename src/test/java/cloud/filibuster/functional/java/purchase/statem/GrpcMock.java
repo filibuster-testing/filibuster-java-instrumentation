012b2346f4f4f9c7aa96c116e3d136388fadc020
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-06-20 22:37:58 -0400

    Add new GRPC mock helper module.

---
+++ src/test/java/cloud/filibuster/functional/java/purchase/statem/GrpcMock.java
@@ -0,0 +1,79 @@
+package cloud.filibuster.functional.java.purchase.statem;
+
+import cloud.filibuster.junit.Assertions;
+import cloud.filibuster.junit.server.core.FilibusterCore;
+import com.google.protobuf.GeneratedMessageV3;
+import io.grpc.MethodDescriptor;
+import org.grpcmock.definitions.verification.CountMatcher;
+import org.grpcmock.definitions.verification.steps.RequestPatternBuilderStep;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.grpcmock.GrpcMock.calledMethod;
+import static org.grpcmock.GrpcMock.times;
+
+public class GrpcMock {
+    private static HashMap<String, Integer> adjustedExpectationsForMethods = new HashMap<>();
+
+    private static HashMap<Object, Integer> adjustedExpectationsForRequests = new HashMap<>();
+
+    public static void resetAdjustedExpectations() {
+        adjustedExpectationsForMethods = new HashMap<>();
+        adjustedExpectationsForRequests = new HashMap<>();
+    }
+
+    public static <ReqT> void adjustExpectation(
+            @Nonnull MethodDescriptor<ReqT, ?> method,
+            @Nonnull int count
+    ) {
+        adjustedExpectationsForMethods.put(method.getFullMethodName(), count);
+    }
+
+    public static <ReqT> void adjustExpectation(
+            @Nonnull MethodDescriptor<ReqT, ?> method,
+            @Nonnull ReqT request,
+            @Nonnull int count
+    ) {
+        adjustedExpectationsForRequests.put(request, count);
+    }
+
+    public static <ReqT> void verifyThat(
+            @Nonnull MethodDescriptor<ReqT, ?> method,
+            @Nonnull int count
+    ) {
+        if (Assertions.wasFaultInjectedOnMethod(method.getFullMethodName())) {
+            if (count > 0) {
+                count = count - 1;
+            }
+        }
+
+        if (adjustedExpectationsForMethods.containsKey(method.getFullMethodName())) {
+            count = adjustedExpectationsForMethods.get(method.getFullMethodName());
+        }
+
+        org.grpcmock.GrpcMock.verifyThat(calledMethod(method), times(count));
+    }
+
+    public static <ReqT> void verifyThat(
+            @Nonnull MethodDescriptor<ReqT, ?> method,
+            @Nonnull ReqT request,
+            @Nonnull int count
+    ) {
+        if (Assertions.wasFaultInjectedOnMethod(method.getFullMethodName())) {
+            if (count > 0) {
+                count = count - 1;
+            }
+        }
+
+        for (Map.Entry<Object, Integer> adjustedExpectationsForRequest : adjustedExpectationsForRequests.entrySet()){
+            if (adjustedExpectationsForRequest.getKey().equals(request)) {
+                count = adjustedExpectationsForRequest.getValue();
+            }
+        }
+
+        org.grpcmock.GrpcMock.verifyThat(calledMethod(method).withRequest(request), times(count));
+    }
+}

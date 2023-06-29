6a80c9b558cb919a18c692547562d1b70c0b4b9f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-06-29 14:22:33 -0400

    Add additional constructor.

--- src/main/java/cloud/filibuster/exceptions/filibuster/FilibusterGrpcTestRuntimeException.java
+++ src/main/java/cloud/filibuster/exceptions/filibuster/FilibusterGrpcTestRuntimeException.java
@@ -5,6 +5,10 @@ public class FilibusterGrpcTestRuntimeException extends FilibusterRuntimeExcepti
         super(error + "\n\t * " + recommendation);
     }
 
+    public FilibusterGrpcTestRuntimeException(String error, String expected, String actual, String recommendation) {
+        super(error + "\n\t * " + recommendation + "\nexpected: " + expected + "\nactual: " + actual);
+    }
+
     public FilibusterGrpcTestRuntimeException(String error, String recommendation, RuntimeException re) {
         super(error + "\n\t * " + recommendation + "\n" + re.getClass().getName() + ": " + re.getMessage());
     }

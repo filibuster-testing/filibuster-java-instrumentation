4b3a0a0b760c524966694294ed705445391ca1d5
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-06-29 13:37:54 -0400

    Add second constructor for thrown runtime exceptions.

--- src/main/java/cloud/filibuster/exceptions/filibuster/FilibusterGrpcTestRuntimeException.java
+++ src/main/java/cloud/filibuster/exceptions/filibuster/FilibusterGrpcTestRuntimeException.java
@@ -4,4 +4,8 @@ public class FilibusterGrpcTestRuntimeException extends FilibusterRuntimeExcepti
     public FilibusterGrpcTestRuntimeException(String error, String recommendation) {
         super(error + "\n\t * " + recommendation);
     }
+
+    public FilibusterGrpcTestRuntimeException(String error, String recommendation, RuntimeException re) {
+        super(error + "\n\t * " + recommendation + "\n" + re.getClass().getName() + ": " + re.getMessage());
+    }
 }

2133961ac0421f8b70e0dc14a7b3dd0f92603af2
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-07-18 20:16:23 -0400

    Alter test to ensure coverage of the full sideEffectingRPC API.

--- src/test/java/cloud/filibuster/functional/java/purchase/tests/scenarios/EndToEndFilibusterDiscountError50GrpcTest.java
+++ src/test/java/cloud/filibuster/functional/java/purchase/tests/scenarios/EndToEndFilibusterDiscountError50GrpcTest.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.util.Map;
 
+import static cloud.filibuster.junit.statem.GrpcMock.stubFor;
+import static cloud.filibuster.junit.statem.GrpcMock.verifyThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EndToEndFilibusterDiscountError50GrpcTest extends EndToEndFilibusterGrpcTest {
@@ -29,6 +31,54 @@ public class EndToEndFilibusterDiscountError50GrpcTest extends EndToEndFilibuste
     @Override
     public void failureBlock() {
         super.failureBlock();
+
+        // State what the state of the system was on UNAVAILABLE.
+        assertOnException(Status.Code.UNAVAILABLE, () -> {
+            // Verify transaction did not occur.
+            assertEquals(20000, PurchaseWorkflow.getAccountBalance(consumerId));
+            assertEquals(0, PurchaseWorkflow.getAccountBalance(merchantId));
+
+            // Notify the system some endpoints are read-only and therefore OK to skip
+            // when we return a failure.
+            readOnlyRPC(UserServiceGrpc.getValidateSessionMethod());
+            readOnlyRPC(CartServiceGrpc.getGetCartForSessionMethod());
+
+            for (Map.Entry<String, String> discountCode : PurchaseWorkflow.getDiscountCodes()) {
+                Hello.GetDiscountRequest request = Hello.GetDiscountRequest.newBuilder()
+                        .setCode(discountCode.getKey())
+                        .build();
+                readOnlyRPC(CartServiceGrpc.getGetDiscountOnCartMethod(), request);
+            }
+
+            sideEffectingRPC(
+                    CartServiceGrpc.getNotifyDiscountAppliedMethod(),
+                    Hello.NotifyDiscountAppliedRequest.newBuilder().setCartId(cartId.toString()).build(),
+                    0);
+        });
+
+        // State what the state of the system was on UNAVAILABLE.
+        assertOnException(Status.Code.DEADLINE_EXCEEDED, () -> {
+            // Verify transaction did not occur.
+            assertEquals(20000, PurchaseWorkflow.getAccountBalance(consumerId));
+            assertEquals(0, PurchaseWorkflow.getAccountBalance(merchantId));
+
+            // Notify the system some endpoints are read-only and therefore OK to skip
+            // when we return a failure.
+            readOnlyRPC(UserServiceGrpc.getValidateSessionMethod());
+            readOnlyRPC(CartServiceGrpc.getGetCartForSessionMethod());
+
+            for (Map.Entry<String, String> discountCode : PurchaseWorkflow.getDiscountCodes()) {
+                Hello.GetDiscountRequest request = Hello.GetDiscountRequest.newBuilder()
+                        .setCode(discountCode.getKey())
+                        .build();
+                readOnlyRPC(CartServiceGrpc.getGetDiscountOnCartMethod(), request);
+            }
+
+            sideEffectingRPC(
+                    CartServiceGrpc.getNotifyDiscountAppliedMethod(),
+                    Hello.NotifyDiscountAppliedRequest.newBuilder().setCartId(cartId.toString()).build(),
+                    0);
+        });
 
         // Combination of three failures using if forces exception.
         CompositeFaultSpecification allCartRequestsFaultSpecification = new CompositeFaultSpecification.Builder()
@@ -59,8 +109,46 @@ public class EndToEndFilibusterDiscountError50GrpcTest extends EndToEndFilibuste
                 readOnlyRPC(CartServiceGrpc.getGetDiscountOnCartMethod(), request);
             }
 
-            sideEffectingRPC(CartServiceGrpc.getNotifyDiscountAppliedMethod(), 0);
+            sideEffectingRPC(
+                    CartServiceGrpc.getNotifyDiscountAppliedMethod(),
+                    Hello.NotifyDiscountAppliedRequest.newBuilder().setCartId(cartId.toString()).build(),
+                    0);
         });
+    }
+
+    @Override
+    public void stubBlock() {
+        stubFor(UserServiceGrpc.getValidateSessionMethod(),
+                Hello.ValidateSessionRequest.newBuilder().setSessionId(sessionId.toString()).build(),
+                Hello.ValidateSessionResponse.newBuilder().build());
+
+        stubFor(UserServiceGrpc.getGetUserFromSessionMethod(),
+                Hello.GetUserRequest.newBuilder().setSessionId(sessionId.toString()).build(),
+                Hello.GetUserResponse.newBuilder().setUserId(consumerId.toString()).build());
+
+        stubFor(CartServiceGrpc.getGetCartForSessionMethod(),
+                Hello.GetCartRequest.newBuilder().setSessionId(sessionId.toString()).build(),
+                Hello.GetCartResponse.newBuilder()
+                        .setCartId(cartId.toString())
+                        .setTotal("10000")
+                        .setMerchantId(merchantId.toString())
+                        .build());
+
+        stubFor(CartServiceGrpc.getGetDiscountOnCartMethod(),
+                Hello.GetDiscountRequest.newBuilder().setCode("FIRST-TIME").build(),
+                Hello.GetDiscountResponse.newBuilder().setPercent("10").build());
+
+        stubFor(CartServiceGrpc.getGetDiscountOnCartMethod(),
+                Hello.GetDiscountRequest.newBuilder().setCode("RETURNING").build(),
+                Hello.GetDiscountResponse.newBuilder().setPercent("5").build());
+
+        stubFor(CartServiceGrpc.getGetDiscountOnCartMethod(),
+                Hello.GetDiscountRequest.newBuilder().setCode("DAILY").build(),
+                Hello.GetDiscountResponse.newBuilder().setPercent("1").build());
+
+        stubFor(CartServiceGrpc.getNotifyDiscountAppliedMethod(),
+                Hello.NotifyDiscountAppliedRequest.newBuilder().setCartId(cartId.toString()).build(),
+                Hello.NotifyDiscountAppliedResponse.newBuilder().build());
     }
 
     @Override
@@ -72,6 +160,26 @@ public class EndToEndFilibusterDiscountError50GrpcTest extends EndToEndFilibuste
                 .setAbortOnLessThanDiscountAmount(50)
                 .build();
         response.set(blockingStub.purchase(request));
+    }
+
+    @Override
+    public void assertStubBlock() {
+        verifyThat(UserServiceGrpc.getValidateSessionMethod(), 1);
+        verifyThat(UserServiceGrpc.getGetUserFromSessionMethod(), 1);
+        verifyThat(CartServiceGrpc.getGetCartForSessionMethod(), 1);
+
+        for (Map.Entry<String, String> discountCode : PurchaseWorkflow.getDiscountCodes()) {
+            Hello.GetDiscountRequest request = Hello.GetDiscountRequest.newBuilder()
+                    .setCode(discountCode.getKey())
+                    .build();
+
+            verifyThat(CartServiceGrpc.getGetDiscountOnCartMethod(), request, 1);
+        }
+
+        verifyThat(
+                CartServiceGrpc.getNotifyDiscountAppliedMethod(),
+                Hello.NotifyDiscountAppliedRequest.newBuilder().setCartId(cartId.toString()).build(),
+                1);
     }
 
     @TestWithFilibuster(

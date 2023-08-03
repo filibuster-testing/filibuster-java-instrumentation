7eb6ed1dbfdb3db78e583130a3689d7211c15cd2
Michael Assad <michael.assad@tum.de>
2023-08-02 21:35:19 -0400

    Annotate shouldInjectFault with @Override

--- src/main/java/cloud/filibuster/junit/filters/WorldServiceFilter.java
+++ src/main/java/cloud/filibuster/junit/filters/WorldServiceFilter.java
@@ -1,6 +1,7 @@
 package cloud.filibuster.junit.filters;
 
 public class WorldServiceFilter implements FilibusterFaultInjectionFilter {
+    @Override
     public boolean shouldInjectFault(String methodName) {
         return !methodName.contains("WorldService");
     }

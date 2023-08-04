53e6326276d1f25572f42cf6900ab5b66dc89df9
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-08-04 17:16:19 -0400

    Fix more tests.

--- src/test/java/cloud/filibuster/integration/instrumentation/libraries/armeria/http/tests/available/FilibusterDecoratingHttpClientToAvailableServiceTest.java
+++ src/test/java/cloud/filibuster/integration/instrumentation/libraries/armeria/http/tests/available/FilibusterDecoratingHttpClientToAvailableServiceTest.java
@@ -229,7 +229,7 @@ public class FilibusterDecoratingHttpClientToAvailableServiceTest extends Filibu
         assertVc.incrementClock(serviceName);
         assertEquals(assertVc.toString(), lastPayload.get("vclock").toString());
 
-        assertEquals("[[\"V1-da39a3ee5e6b4b0d3255bfef95601890afd80709-4cf5bc59bee9e1c44c6254b5f84e7f066bd8e5fe-a468b76d6940d5e59a854b8c01bb25e7e202be04-db082cf8db1dc42c9c3f534ca27df61d9d002893-00aa7adca5809bf3003b7469bdf22140ac380041\", 1], [\"V1-da39a3ee5e6b4b0d3255bfef95601890afd80709-aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d-bf801c417a24769c151e3729f35ee3e62e4e04d4-c1fcccb124d5830d301226ffad692a3bb59602ab-0a8127ad87a9ab6630533750add635c52d003488\", 1]]", lastPayload.getString("execution_index"));
+        assertEquals("[[\"V1-da39a3ee5e6b4b0d3255bfef95601890afd80709-4cf5bc59bee9e1c44c6254b5f84e7f066bd8e5fe-a468b76d6940d5e59a854b8c01bb25e7e202be04-db082cf8db1dc42c9c3f534ca27df61d9d002893-00aa7adca5809bf3003b7469bdf22140ac380041\", 1], [\"V1-da39a3ee5e6b4b0d3255bfef95601890afd80709-aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d-bf801c417a24769c151e3729f35ee3e62e4e04d4-c1fcccb124d5830d301226ffad692a3bb59602ab-efcac005fd5764253ef51e64f89f80aa3694defe\", 1]]", lastPayload.getString("execution_index"));
 
         // Success values.
         returnValue = lastPayload.getJSONObject("return_value");

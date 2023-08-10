87467e29b1ded45a0cfb28518e33d3e9dda8ac96
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-08-09 23:54:22 -0400

    Add javadoc.

--- src/main/java/cloud/filibuster/junit/assertions/protocols/HttpAssertions.java
+++ src/main/java/cloud/filibuster/junit/assertions/protocols/HttpAssertions.java
@@ -6,7 +6,19 @@ import static cloud.filibuster.instrumentation.helpers.Property.getServerBackend
 import static cloud.filibuster.junit.assertions.protocols.GenericAssertions.wasFaultInjectedHelper;
 
 public class HttpAssertions {
-    // TODO
+
+    public static class HttpAssertionsNotImplementedException extends RuntimeException {
+
+    }
+
+    /**
+     * Returns true if a fault was injected on a HTTP service.
+     *
+     * <p>Requires that the invoked side uses the {@link cloud.filibuster.instrumentation.instrumentors.FilibusterServerInstrumentor FilibusterServerInstrumentor} in a {@link cloud.filibuster.instrumentation.libraries.armeria.http.FilibusterDecoratingHttpService FilibusterDecoratingHttpService}.</p>
+     *
+     * @param serviceName string service name
+     * @return if a fault was injected
+     */
     public static boolean wasFaultInjectedOnService(String serviceName) {
         if (getServerBackendCanInvokeDirectlyProperty()) {
             if (FilibusterCore.hasCurrentInstance()) {
@@ -17,5 +29,20 @@ public class HttpAssertions {
         } else {
             return wasFaultInjectedHelper("/filibuster/fault-injected/service/" + serviceName);
         }
+    }
+
+    // TODO: add javadoc once implemented
+    public static boolean wasFaultInjectedOnMethod(String httpMethod) {
+        throw new HttpAssertionsNotImplementedException();
+    }
+
+    // TODO: add javadoc once implemented
+    public static boolean wasFaultInjectedOnMethod(String httpMethod, String URI) {
+        throw new HttpAssertionsNotImplementedException();
+    }
+
+    // TODO: add javadoc once implemented
+    public static boolean wasFaultInjectedOnRequest(String request) {
+        throw new HttpAssertionsNotImplementedException();
     }
 }

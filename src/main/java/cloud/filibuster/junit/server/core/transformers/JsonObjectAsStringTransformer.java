db20ca9a1b809528515999d714d8193ead5e99a2
Michael Assad <michael.assad@tum.de>
2023-08-13 18:42:04 -0400

    WIP: Attach transformers to JSONObject fields

--- src/main/java/cloud/filibuster/junit/server/core/transformers/JsonObjectAsStringTransformer.java
+++ src/main/java/cloud/filibuster/junit/server/core/transformers/JsonObjectAsStringTransformer.java
@@ -1,38 +1,63 @@
 package cloud.filibuster.junit.server.core.transformers;
 
+import cloud.filibuster.exceptions.filibuster.FilibusterTransformerException;
 import cloud.filibuster.exceptions.transformer.TransformerNullResultException;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.json.JSONObject;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 
-public final class JsonObjectAsStringTransformer implements Transformer<String, ArrayList<String>> {
+import static cloud.filibuster.junit.server.core.FilibusterCoreTransformerExtension.getTransformerInstance;
+import static cloud.filibuster.junit.server.core.transformers.selector.GatewayTransformer.getTransformerClassNameFromReferenceValue;
+
+public final class JsonObjectAsStringTransformer implements Transformer<String, ArrayList<SimpleImmutableEntry<String, String>>> {
     private boolean hasNext = true;
     private String result;
-    private Accumulator<String, ArrayList<String>> accumulator;
+    private Accumulator<String, ArrayList<SimpleImmutableEntry<String, String>>> accumulator;
+    private Transformer<?, ?> lastTransformationResult;
+    private static final Gson gson = new Gson();
 
     @Override
     @CanIgnoreReturnValue
-    public JsonObjectAsStringTransformer transform(String payload, Accumulator<String, ArrayList<String>> accumulator) {
-        ArrayList<String> ctx = accumulator.getContext();
+    public JsonObjectAsStringTransformer transform(String payload, Accumulator<String, ArrayList<SimpleImmutableEntry<String, String>>> accumulator) {
+        ArrayList<SimpleImmutableEntry<String, String>> ctx = accumulator.getContext();
 
         JSONObject payloadJO = new JSONObject(payload);
 
-        // If the context is empty, add the first key in the payload to the context
-        // That is the case in the first iteration
-        if (ctx.size() == 0 && payloadJO.keySet().size() > 0) {
-            ctx.add(payloadJO.keySet().iterator().next());
-            accumulator.setContext(ctx);
+        SimpleImmutableEntry<String, String> lastCtxEntry = ctx.get(ctx.size() - 1);
+
+        String transformerClassName = getTransformerClassNameFromReferenceValue(String.class.getName(), lastCtxEntry.getKey());
+        Transformer<?, ?> lastTransformer = getTransformerInstance(transformerClassName);
+        Accumulator<?, ?> lastAccumulator = gson.fromJson(lastCtxEntry.getValue(), lastTransformer.getAccumulatorType());
+
+        try {
+            Method transformMethod = lastTransformer.getClass().getMethod("transform", (Class<?>) lastTransformer.getPayloadType(), Accumulator.class);
+
+            lastTransformationResult =
+                    (Transformer<?, ?>) transformMethod.invoke(
+                            lastTransformer,
+                            payloadJO.getString(lastCtxEntry.getKey()),
+                            lastAccumulator
+                    );
+            payloadJO.put(lastCtxEntry.getKey(), lastTransformationResult.getResult().toString());
+            if (lastTransformationResult.hasNext()) {
+                lastAccumulator = lastTransformationResult.getNextAccumulator();
+                ctx.set(ctx.size() - 1, new SimpleImmutableEntry<>(lastCtxEntry.getKey(), gson.toJson(lastAccumulator)));
+            }
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new FilibusterTransformerException("[JsonObjectAsStringTransformer]: An exception occurred while invoking transform method of transformer " + lastTransformer.getClass().getName(), e);
         }
 
         // If the context has all the keys in the payload, set hasNext to false
-        if (ctx.size() == payloadJO.keySet().size()) {
+        if (ctx.size() == payloadJO.keySet().size() && !lastTransformationResult.hasNext()) {
             this.hasNext = false;
         }
-
-        payloadJO.remove(ctx.get(ctx.size() - 1));  // Remove the last key in the ctx from the payload
 
         this.result = payloadJO.toString();
         this.accumulator = accumulator;
@@ -60,26 +85,38 @@ public final class JsonObjectAsStringTransformer implements Transformer<String, 
 
     @Override
     public Type getAccumulatorType() {
-        Type listType = TypeToken.getParameterized(ArrayList.class, String.class).getType();
+        Type stringType = TypeToken.get(String.class).getType();
+        Type simpleEntryType = TypeToken.getParameterized(SimpleImmutableEntry.class, String.class, String.class).getType();
+        Type listType = TypeToken.getParameterized(ArrayList.class, simpleEntryType).getType();
 
         return TypeToken.getParameterized(
                 Accumulator.class,
-                String.class,
+                stringType,
                 listType).getType();
     }
 
     @Override
-    public Accumulator<String, ArrayList<String>> getInitialAccumulator(String referenceValue) {
+    public Accumulator<String, ArrayList<SimpleImmutableEntry<String, String>>> getInitialAccumulator(String referenceValue) {
         // Prepare initial context
-        ArrayList<String> ctx = new ArrayList<>();
+        ArrayList<SimpleImmutableEntry<String, String>> ctx = new ArrayList<>();
         JSONObject referenceJO = new JSONObject(referenceValue);
-        if (referenceJO.keySet().size() > 0) {  // If the reference value is an empty JSON object, do not add anything to the context
-            ctx.add(referenceJO.keySet().iterator().next());
+        // If the reference value is an empty JSON object, do not add anything to the context
+        if (referenceJO.keySet().size() > 0) {
+            String firstKey = referenceJO.keySet().iterator().next();
+            String firstValue = referenceJO.getString(firstKey);
+            String transformerClassName = getTransformerClassNameFromReferenceValue(String.class.getName(), firstValue);
+
+            Transformer<?, ?> transformerObject = getTransformerInstance(transformerClassName);
+            Accumulator<?, ?> initialAccumulator = transformerObject.getInitialAccumulator(gson.fromJson(firstValue, transformerObject.getPayloadType()));
+
+
+            SimpleImmutableEntry<String, String> entry = new SimpleImmutableEntry<>(firstKey, gson.toJson(initialAccumulator));
+            ctx.add(entry);
         } else {
             this.hasNext = false;
         }
 
-        Accumulator<String, ArrayList<String>> accumulator = new Accumulator<>();
+        Accumulator<String, ArrayList<SimpleImmutableEntry<String, String>>> accumulator = new Accumulator<>();
         accumulator.setContext(ctx);
         accumulator.setReferenceValue(referenceValue);
         this.result = referenceValue;
@@ -87,20 +124,28 @@ public final class JsonObjectAsStringTransformer implements Transformer<String, 
     }
 
     @Override
-    public Accumulator<String, ArrayList<String>> getNextAccumulator() {
+    public Accumulator<String, ArrayList<SimpleImmutableEntry<String, String>>> getNextAccumulator() {
         if (this.accumulator == null) {
             return getInitialAccumulator(getResult());
         } else {
-            ArrayList<String> ctx = accumulator.getContext();
-            JSONObject referenceJO = new JSONObject(accumulator.getReferenceValue());
+            if (!lastTransformationResult.hasNext()) {
+                ArrayList<SimpleImmutableEntry<String, String>> ctx = accumulator.getContext();
+                JSONObject referenceJO = new JSONObject(accumulator.getReferenceValue());
+                if (ctx.size() == referenceJO.keySet().size()) {
+                    this.hasNext = false;
+                } else {
+                    String nextKey = new ArrayList<>(referenceJO.keySet()).get(ctx.size());
+                    String nextValue = referenceJO.getString(nextKey);
+                    String transformerClassName = getTransformerClassNameFromReferenceValue(String.class.getName(), nextValue);
 
-            for (String key : referenceJO.keySet()) {
-                if (!ctx.contains(key)) {
-                    ctx.add(key);
-                    break;
+                    Transformer<?, ?> transformerObject = getTransformerInstance(transformerClassName);
+                    Accumulator<?, ?> initialAccumulator = transformerObject.getInitialAccumulator(gson.fromJson(nextValue, transformerObject.getPayloadType()));
+
+                    SimpleImmutableEntry<String, String> entry = new SimpleImmutableEntry<>(nextKey, gson.toJson(initialAccumulator));
+                    ctx.add(entry);
+                    accumulator.setContext(ctx);
                 }
             }
-            accumulator.setContext(ctx);
             return accumulator;
         }
     }
---
+++ src/test/java/cloud/filibuster/functional/database/redis/JUnitRedisFilibusterGatewayTransformer2Test.java
@@ -0,0 +1,79 @@
+package cloud.filibuster.functional.database.redis;
+
+import cloud.filibuster.functional.java.JUnitAnnotationBaseTest;
+import cloud.filibuster.instrumentation.libraries.dynamic.proxy.DynamicProxyInterceptor;
+import cloud.filibuster.integration.examples.armeria.grpc.test_services.RedisClientService;
+import cloud.filibuster.junit.TestWithFilibuster;
+import cloud.filibuster.junit.configuration.examples.db.redis.RedisGatewayTransformerAnalysisConfigurationFile;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static cloud.filibuster.junit.assertions.protocols.GenericAssertions.wasFaultInjected;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class JUnitRedisFilibusterGatewayTransformer2Test extends JUnitAnnotationBaseTest {
+    static final String jsonKey = "jsonKey";
+    static final String jsonValue = new JSONObject().put("hello", "world").put("foo", "bar").toString();
+    static StatefulRedisConnection<String, String> statefulRedisConnection;
+    static String redisConnectionString;
+    private final static Set<String> testExceptionsThrown = new HashSet<>();
+
+    private static int numberOfTestExecutions = 0;
+
+    @BeforeAll
+    public static void primeCache() {
+        statefulRedisConnection = RedisClientService.getInstance().redisClient.connect();
+        redisConnectionString = RedisClientService.getInstance().connectionString;
+        statefulRedisConnection.sync().set(jsonKey, jsonValue);
+    }
+
+    @DisplayName("Tests Redis gateway transformer for Strings.")
+    @Order(1)
+    @TestWithFilibuster(analysisConfigurationFile = RedisGatewayTransformerAnalysisConfigurationFile.class)
+    public void testRedisGatewayStringTransformation() {
+        try {
+            numberOfTestExecutions++;
+
+            StatefulRedisConnection<String, String> myStatefulRedisConnection = DynamicProxyInterceptor.createInterceptor(statefulRedisConnection, redisConnectionString);
+            RedisCommands<String, String> myRedisCommands = myStatefulRedisConnection.sync();
+
+            String returnVal = myRedisCommands.get(jsonKey);
+            assertEquals(jsonValue, returnVal);
+
+            assertFalse(wasFaultInjected());
+        } catch (Throwable t) {
+            testExceptionsThrown.add(t.getMessage());
+
+            assertTrue(wasFaultInjected(), "An exception was thrown although no fault was injected: " + t);
+        }
+    }
+
+//    @DisplayName("Verify correct number of test executions.")
+//    @Test
+//    @Order(2)
+//    // TODO
+//    public void testNumExecutions() {
+//        assertEquals(10, numberOfTestExecutions);
+//    }
+//
+//    @DisplayName("Verify correct number of faults.")
+//    @Test
+//    @Order(3)
+//    // TODO
+//    public void testNumExceptions() {
+//        assertEquals(10, testExceptionsThrown.size());
+//    }
+
+}

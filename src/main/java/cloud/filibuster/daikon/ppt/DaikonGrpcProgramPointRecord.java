f7221ed41be213c1ac5d64e9c469006bb7f338af
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-09-18 11:14:28 -0400

    Update daikon algorithm.

--- src/main/java/cloud/filibuster/daikon/ppt/DaikonGrpcProgramPointRecord.java
+++ src/main/java/cloud/filibuster/daikon/ppt/DaikonGrpcProgramPointRecord.java
@@ -4,45 +4,90 @@ import cloud.filibuster.daikon.DaikonPptType;
 import cloud.filibuster.exceptions.filibuster.FilibusterRuntimeException;
 import cloud.filibuster.junit.server.core.transformers.JsonUtils;
 import com.google.protobuf.GeneratedMessageV3;
-import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import org.json.JSONObject;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 import static cloud.filibuster.daikon.DaikonPpt.generatePpt;
 
 public class DaikonGrpcProgramPointRecord {
     private final String ppt;
     private final DaikonPptType daikonPptType;
-    private List<DaikonGrpcProgramPointVariable> variables = new ArrayList<>();
+    private final List<DaikonGrpcProgramPointVariable> variables;
 
-    public DaikonGrpcProgramPointRecord(String ppt, DaikonPptType daikonPptType, GeneratedMessageV3 generatedMessageV3) {
-        this.ppt = ppt;
+    public DaikonGrpcProgramPointRecord(String fullMethodName, DaikonPptType daikonPptType, GeneratedMessageV3 requestMessage, @Nullable GeneratedMessageV3 responseMessage) {
+        this.ppt = generatePpt(fullMethodName, daikonPptType, requestMessage);
         this.daikonPptType = daikonPptType;
 
         try {
-            String serializedMessage = JsonFormat.printer().preservingProtoFieldNames().includingDefaultValueFields().print(generatedMessageV3);
-            JSONObject jsonObject = new JSONObject(serializedMessage);
 
-            // Flatten to uniformly (easily) handle nested GRPC messages.
-            jsonObject = JsonUtils.flatten(jsonObject);
-
-            // Iterate all the keys.
-            Iterator<String> keys = jsonObject.keys();
             List<DaikonGrpcProgramPointVariable> variables = new ArrayList<>();
 
-            while(keys.hasNext()) {
-                String key = keys.next();
-                variables.add(new DaikonGrpcProgramPointVariable(key));
+            if (daikonPptType.equals(DaikonPptType.ENTER) || daikonPptType.equals(DaikonPptType.EXIT)) {
+                variables.add(new DaikonGrpcProgramPointVariable(
+                        "this",
+                        "variable",
+                        null,
+                        "java.lang.Object",
+                        "hashcode",
+                        "is_param nomod",
+                        "1"));
+
+                String serializedMessage = JsonFormat.printer().preservingProtoFieldNames().includingDefaultValueFields().print(requestMessage);
+                JSONObject jsonObject = new JSONObject(serializedMessage);
+                jsonObject = JsonUtils.flatten(jsonObject);
+                Iterator<String> keys = jsonObject.keys();
+
+                while(keys.hasNext()) {
+                    String key = keys.next();
+                    variables.add(new DaikonGrpcProgramPointVariable(
+                            "this." + key,
+                            "field " + key,
+                            "this",
+                            "java.lang.String",
+                            "java.lang.String",
+                            "nomod not_ordered to_string",
+                            "1"));
+                }
+            }
+
+            if (daikonPptType.equals(DaikonPptType.EXIT)) {
+                variables.add(new DaikonGrpcProgramPointVariable(
+                        "return",
+                        "return",
+                        null,
+                        "java.lang.Object",
+                        "hashcode",
+                        null,
+                        "1"));
+
+                String serializedMessage = JsonFormat.printer().preservingProtoFieldNames().includingDefaultValueFields().print(responseMessage);
+                JSONObject jsonObject = new JSONObject(serializedMessage);
+                jsonObject = JsonUtils.flatten(jsonObject);
+                Iterator<String> keys = jsonObject.keys();
+
+                while(keys.hasNext()) {
+                    String key = keys.next();
+                    variables.add(new DaikonGrpcProgramPointVariable(
+                            "return." + key,
+                            "field " + key,
+                            "return",
+                            "java.lang.String",
+                            "java.lang.String",
+                            "not_ordered to_string",
+                            "1"));
+                }
             }
 
             this.variables = variables;
-        } catch (InvalidProtocolBufferException e) {
-            throw new FilibusterRuntimeException(e);
+        } catch (Throwable t) {
+            throw new FilibusterRuntimeException(t);
         }
     }
 
@@ -55,20 +100,43 @@ public class DaikonGrpcProgramPointRecord {
         for (DaikonGrpcProgramPointVariable variable : variables) {
             builder.append("variable " + variable.getName() + "\n");
             builder.append("  var-kind " + variable.getVarKind() + "\n");
+            if (variable.getEnclosingVar() != null) {
+                builder.append("  enclosing-var " + variable.getEnclosingVar() + "\n");
+            }
             builder.append("  dec-type " + variable.getDecType() + "\n");
             builder.append("  rep-type " + variable.getRepType() + "\n");
-            builder.append("  flags " + variable.getFlags() + "\n");
+            if (variable.getFlags() != null) {
+                builder.append("  flags " + variable.getFlags() + "\n");
+            }
             builder.append("  comparability " + variable.getComparability() + "\n");
         }
 
         return builder.toString();
     }
 
-    public static DaikonGrpcProgramPointRecord onRequest(String fullMethodName, GeneratedMessageV3 request) {
-        return new DaikonGrpcProgramPointRecord(generatePpt(fullMethodName, DaikonPptType.ENTER, request), DaikonPptType.ENTER, request);
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.ppt);
     }
 
-    public static DaikonGrpcProgramPointRecord onResponse(String fullMethodName, GeneratedMessageV3 response) {
-        return new DaikonGrpcProgramPointRecord(generatePpt(fullMethodName, DaikonPptType.EXIT, response), DaikonPptType.EXIT, response);
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        if (!(o instanceof DaikonGrpcProgramPointRecord)) {
+            return false;
+        }
+
+        DaikonGrpcProgramPointRecord r = (DaikonGrpcProgramPointRecord) o;
+        return Objects.equals(this.ppt, r.ppt);
+    }
+
+    public static List<DaikonGrpcProgramPointRecord> onRequestAndResponse(String fullMethodName, GeneratedMessageV3 request, GeneratedMessageV3 response) {
+        List<DaikonGrpcProgramPointRecord> declRecords = new ArrayList<>();
+        declRecords.add(new DaikonGrpcProgramPointRecord(fullMethodName, DaikonPptType.ENTER, request, null));
+        declRecords.add(new DaikonGrpcProgramPointRecord(fullMethodName, DaikonPptType.EXIT, request, response));
+        return declRecords;
     }
 }

744aa2919241ceb8d2c2f137be5543e67e555d0a
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2023-09-18 11:14:40 -0400

    Update program points to use java.lang.Object.

--- src/main/java/cloud/filibuster/daikon/DaikonPpt.java
+++ src/main/java/cloud/filibuster/daikon/DaikonPpt.java
@@ -9,14 +9,13 @@ public class DaikonPpt {
 
     public static String generatePpt(String fullMethodName, DaikonPptType daikonPptType, GeneratedMessageV3 generatedMessageV3) {
         // Update program point name.
-        String messageName = generatedMessageV3.getDescriptorForType().getName();
         fullMethodName = fullMethodName.replace("/", ".");
         String ppt;
 
         if (daikonPptType.equals(DaikonPptType.EXIT)) {
-            ppt = fullMethodName + "(" + messageName + "):::" + daikonPptType + "0"; // assume single exit point for GRPC for now, no way to really know currently.
+            ppt = fullMethodName + "(java.lang.Object):::" + daikonPptType + "0"; // assume single exit point for GRPC for now, no way to really know currently.
         } else {
-            ppt = fullMethodName + "(" + messageName + "):::" + daikonPptType;
+            ppt = fullMethodName + "(java.lang.Object):::" + daikonPptType;
         }
 
         return ppt;
